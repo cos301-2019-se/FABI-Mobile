823b21118ad2affa5131d8d87e0134c388672f72
Tegan <u17034729@tuks.co.za>
2019-05-10 20:37:36 +0200

    Added CSS to Web App

--- FABI-Web/src/app/login/login.component.ts
+++ FABI-Web/src/app/login/login.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+//import { LoginInfo } from '../apiconnection.service';
+//import { APIconnectionService } from '../apiconnection.service';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material';
+import { MatDialog } from '@angular/material';
+import { ErrorComponent } from '../error/error.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
