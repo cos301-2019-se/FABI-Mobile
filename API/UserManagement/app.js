0388bd1c7e7eb9dc28a6e25ed1d43b65822c7c7e
aeronland <aeronland@gmail.com>
2019-05-21 01:36:54 +0200

    Added addGuest, addStaff and createOrganization

--- API/UserManagement/app.js
+++ API/UserManagement/app.js
@@ -7,6 +7,9 @@ const fs = require('fs');
 const authenticatePublicUserRoute = require('./api/routes/authenticatePublicUser');
 const displayHTML = require('./api/routes/displayHTML');
 const addPublicUserRoute = require('./api/routes/addPublicUser');
+const createOrganizationRoute = require('./api/routes/createOrganization');
+const addGuestToOrgRoute = require('./api/routes/addGuestToOrg');
+const addStaffRoute = require('./api/routes/addStaff');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -16,8 +19,12 @@ app.use(cors());
 
 //Send any requests with authenticateNFC to the correct route
 app.use('/authenticatePublicUser', authenticatePublicUserRoute);
-app.use('/',displayHTML);
 app.use('/addPublicUser',addPublicUserRoute);
+
+app.use('/',displayHTML);
+app.use('/createOrganization', createOrganizationRoute);
+app.use('/addGuestToOrg', addGuestToOrgRoute);
+app.use('/addStaff', addStaffRoute);
 
 //Error handling when url doesn't exist
 // app.use((req, res, next) => {
