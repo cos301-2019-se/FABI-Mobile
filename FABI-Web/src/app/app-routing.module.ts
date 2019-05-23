c6949f92fcadc9cd960ee0d771548db4b15bd0af
Kendra <kendrar@mweb.co.za>
2019-05-23 16:33:08 +0200

    Login

--- FABI-Web/src/app/app-routing.module.ts
+++ FABI-Web/src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SampleFormComponent } from './sample-form/sample-form.component';
+import { AdminModule} from './Admin/admin.module';
+import { AdminDashboardComponent } from "./Admin/admin-dashboard/admin-dashboard.component";
+
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'sample-form', component: SampleFormComponent},
-  {path: '', component: LoginComponent}
+  {path: '', component: LoginComponent},
+  {path: 'Admin', component: AdminModule,
+    children :[
+      { path: 'admin-dashboard', component: AdminDashboardComponent}
+  ]}
 ];
 
 @NgModule({
