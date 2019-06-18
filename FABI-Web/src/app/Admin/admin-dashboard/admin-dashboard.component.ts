71492c37e4287b14614cc3f51232cc2f59eff948
Emma <u17074526@tuks.co.za>
2019-06-18 20:41:22 +0200

    Updated Menu and Dashboard

--- FABI-Web/src/app/Admin/admin-dashboard/admin-dashboard.component.ts
+++ FABI-Web/src/app/Admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-//import { SidenavComponent } from '../../sidenav/sidenav.component';
+import { Component, OnInit, isDevMode, Inject } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { DomSanitizer } from '@angular/platform-browser';
+import { sharedStylesheetJitUrl } from '@angular/compiler';
+
+declare var require: any;
 
 @Component({
   selector: 'app-admin-dashboard',
-  templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.scss']
+  templateUrl: './admin-dashboard.component.html'
 })
 
 export class AdminDashboardComponent implements OnInit {
-  constructor() { }
+  navWidth: string;
+
+  constructor(public sanitizer: DomSanitizer) { }
   
   sidenavToggle(){
-    if(document.getElementById("sidenav_div").style.width == "22%")
+    if(document.getElementById("sidenav_div").style.width == this.navWidth)
     {
       document.getElementById("sidenav_div").style.width = "0";
     }
     else{
-      document.getElementById("sidenav_div").style.width = "22%";
+      document.getElementById("sidenav_div").style.width = this.navWidth;
     } 
   }
 
@@ -24,6 +30,35 @@ export class AdminDashboardComponent implements OnInit {
     document.getElementById("sidenav_div").style.width = "0";
   }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    if(window.innerWidth <= 520){
+      //Mobile device
+      require("style-loader!./../../../assets/admin_dashboard_styles/mobile_style.scss");
+      this.navWidth = "100%";
+    }
+    else if(window.innerWidth <= 1400 && window.innerWidth > 520){
+      //Tablet device
+      require("style-loader!./../../../assets/admin_dashboard_styles/tablet_style.scss");
 
+      if(window.innerWidth > 920 && window.innerWidth < 1000){
+        this.navWidth = "50%";
+      }
+      else if(window.innerWidth >= 1000 && window.innerWidth < 1050){
+        this.navWidth = "40%";
+      }
+      else{
+        this.navWidth = "34%";
+      }
+    }
+    else if(window.innerWidth <= 2500 && window.innerWidth > 1400){
+      //Laptop device
+      require("style-loader!./admin-dashboard.component.scss");
+      this.navWidth = "22%";
+    }
+    else if(window.innerWidth > 2500){
+      //Desktop device
+      require("style-loader!./../../../assets/admin_dashboard_styles/desktop_style.scss");
+      this.navWidth = "70%";
+    }
+  }
 }
