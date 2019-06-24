f4e7faed14ff5bfc1a0c6b0196f5dd1842ad2cf1
aeronland <aeronland@gmail.com>
2019-06-25 01:31:02 +0200

    added remove for members, staff and administrators. Edited format of response text

--- API/UserManagement/api/routes/External/addMemberToOrg.js
+++ API/UserManagement/api/routes/External/addMemberToOrg.js
@@ -19,9 +19,9 @@ router.post('/', addMember);
  * @description  REQUEST DATA REQUIRED: Organization name, Member name, email, surname
  *  1. Check if all required data is received and that it is correct.
  *      - IF NOT: return Error Response
- *  2. Connect to DB.
+ *  2. Generate and Encrypt Password.
+ *  3. Connect to DB.
  *      - IF ERROR: return Error Response
- *  3. Encrypt Password.
  *  4. Add Member to given organization
  *      - IF ERROR: return Error Response
  *  5. Send appropriate response message.
@@ -33,31 +33,20 @@ router.post('/', addMember);
 
 // [START config]
 const db = admin.firestore();
-
+//[END config]
 function addMember(req, res)
 {
-// Store request data is qs
-    const salt = bcrypt.genSaltSync(10);
-    var pass = generatePassword(10);
-    const qs = {
-        fname: req.body.member.name,
-        surname: req.body.member.surname,
-        email: req.body.member.email,
-        password: bcrypt.hashSync(pass, salt)
-    }
 
-// (1) Check if all required data is received and that it is correct.
+// (1)
     if (req.body.member.name == undefined || req.body.member.name == '') {
         res.setHeader('Content-Type', 'application/problem+json');
         res.setHeader('Content-Language', 'en');
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.status(400).json({                                  // ******* RESPONSE STATUS? ************
             success: false,
-            error: {
-                code: 400,
-                title: "BAD_REQUEST",
-                message: "User name expected"
-            }
+            code: 400,
+            title: "BAD_REQUEST",
+            message: "User name expected"
         });
     }
     if (req.body.member.surname == undefined || req.body.member.surname == '') {
@@ -66,11 +55,9 @@ function addMember(req, res)
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.status(400).json({                                  // ******* RESPONSE STATUS? ************
             success: false,
-            error: {
-                code: 400,
-                title: "BAD_REQUEST",
-                message: "User surname expected"
-            }
+            code: 400,
+            title: "BAD_REQUEST",
+            message: "User surname expected"
         });
     }
     if (req.body.member.email == undefined || req.body.member.email == '') {
@@ -79,11 +66,9 @@ function addMember(req, res)
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.status(400).json({                                  // ******* RESPONSE STATUS? ************
             success: false,
-            error: {
-                code: 400,
-                title: "BAD_REQUEST",
-                message: "User email expected"
-            }
+            code: 400,
+            title: "BAD_REQUEST",
+            message: "User email expected"
         });
     }
     if (req.body.orgName == undefined || req.body.member.orgName == '') {
@@ -92,55 +77,51 @@ function addMember(req, res)
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.status(400).json({                                  // ******* RESPONSE STATUS? ************
             success: false,
-            error: {
-                code: 400,
-                title: "BAD_REQUEST",
-                message: "orgName expected"
-            }
+            code: 400,
+            title: "BAD_REQUEST",
+            message: "orgName expected"
         });
     }
 
-    // Check if valid email format   (************ CLIENT SIDE? ***************)
-    // var regEx = [A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/igm;
-    // "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$"
-    // "^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$"
-    // if(!regEx.test(qs.email)) {
-    //     res.setHeader('Content-Type', 'applicagion/problem+json');
-    //     res.setHeader('Content-Language', 'en');
-    //     res.status(400).json({                                  // ******* RESPONSE STATUS? ************
-    //         success: false,
-    //         error: {
-    //             code: 400,
-    //             title: "BAD_REQUEST",
-    //             message: "User password expected"
-    //         }
-    //     });
-    // }
-
-
-// (2) Connect to DB
-
+    // (2)
+    const salt = bcrypt.genSaltSync(10);
+    var pass = generatePassword(10);
+    const qs = {
+        fname: req.body.member.name,
+        surname: req.body.member.surname,
+        email: req.body.member.email,
+        password: bcrypt.hashSync(pass, salt)
+    }
+    
+    // (3)
     var docRef  = db.collection('Organizations').doc(req.body.orgName).collection('Members').doc(qs.email);
+    
+    
+    
+    //(4)
     docRef.set(qs).then(() => {
+
+    //(5)
         res.setHeader('Content-Type', 'application/problem+json');
-    res.setHeader('Content-Language', 'en');
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(200).json({                                  // ******* RESPONSE STATUS? ************
+        res.setHeader('Content-Language', 'en');
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.status(200).json({                                  // ******* RESPONSE STATUS? ************
         success: true,
-        data: {
-            code: 200,
+        code: 200,
             title: "SUCCESS",
             message: "Added Member",
-            content: {message : "Member Added to Organization",
+            data: {
+                message : "Member Added to Organization",
                 orgName : req.body.orgName,
                 tempPassword : pass}
-        }
+        
     });
     mail(req.body.orgName + ' Member', pass);
 });
 
 }
 
+//Function to generate password
 function generatePassword(length) {
     var result           = '';
     var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
