eb3514beae8440f14e3d0fe33ec3d8be1456db17
Tegan <u17034729@tuks.co.za>
2019-06-25 13:58:28 +0200

    Removed invalid file

--- FABI-Web/src/app/app-routing.module.ts
+++ FABI-Web/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from "./login/login.component";
-// import { SampleFormComponent } from './sample-form/sample-form.component';
+import { HomeComponent } from "./home/home.component";
 import { AdminDashboardComponent } from "./Admin/admin-dashboard/admin-dashboard.component";
 import { OrganizationDashboardComponent } from "./Organization/organization-dashboard/organization-dashboard.component";
 import { StaffDashboardComponent } from "./Staff/staff-dashboard/staff-dashboard.component";
@@ -11,15 +11,12 @@ import { MemberDashboardComponent } from "./Organization-Member/member-dashboard
 
 
 const routes: Routes = [
+  {path: ' ', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  // {path: 'sample-form', component: SampleFormComponent},
-  {path: '', component: LoginComponent},
   {path: 'fabi-admin-dashboard', component: AdminDashboardComponent},
   {path: 'org-admin-dashboard', component: OrganizationDashboardComponent},
   {path: 'fabi-staff-dashboard', component: StaffDashboardComponent},
   {path: 'org-member-dashboard', component: MemberDashboardComponent}
-
-
 
 ];
 
