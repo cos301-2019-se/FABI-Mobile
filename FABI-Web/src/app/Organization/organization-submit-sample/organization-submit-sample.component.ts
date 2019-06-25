ac7f06f8371c5f715a19e1418248eb2df3d3298f
Kendra <kendrar@mweb.co.za>
2019-06-25 13:11:52 +0200

    Demo Backend Code

--- FABI-Web/src/app/Organization/organization-submit-sample/organization-submit-sample.component.ts
+++ FABI-Web/src/app/Organization/organization-submit-sample/organization-submit-sample.component.ts
@@ -1,4 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+/**
+ * File Name: organization-submit-sample.component.ts
+ * File Path: c:\Users\Kendra\Documents\Varsity\Third Year\COS301\CAPSTONE\Git Repo\FABI-Mobile\FABI-Web\src\app\Organization\organization-submit-sample\organization-submit-sample.component.ts
+ * Project Name: fabi-web
+ * Created Date: Friday, May 24th 2019
+ * Author: Team Nova - novacapstone@gmail.com
+ * -----
+ * Last Modified: Tuesday, June 25th 2019
+ * Modified By: Team Nova
+ * -----
+ * Copyright (c) 2019 University of Pretoria
+ * 
+ * <<license>>
+ */
+
+
+1import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-organization-submit-sample',
