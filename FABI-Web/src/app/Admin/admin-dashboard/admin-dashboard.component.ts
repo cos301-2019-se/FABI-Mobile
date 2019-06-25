0d302c1e42d8f7bd8fb38410f43b3078ac418fd8
Tegan <u17034729@tuks.co.za>
2019-06-25 13:58:28 +0200

    Removed invalid file

--- FABI-Web/src/app/Admin/admin-dashboard/admin-dashboard.component.ts
+++ FABI-Web/src/app/Admin/admin-dashboard/admin-dashboard.component.ts
@@ -34,13 +34,13 @@ export class AdminDashboardComponent implements OnInit {
    * @memberof AdminDashboardComponent
    */
   sidenavToggle(){
-    if(document.getElementById("sidenav_div").style.width == this.navWidth)
-    {
-      document.getElementById("sidenav_div").style.width = "0";
-    }
-    else{
-      document.getElementById("sidenav_div").style.width = this.navWidth;
-    } 
+    // if(document.getElementById("sidenav_div").style.width == this.navWidth)
+    // {
+    //   document.getElementById("sidenav_div").style.width = "0";
+    // }
+    // else{
+    //   document.getElementById("sidenav_div").style.width = this.navWidth;
+    // } 
   }
 
   closeNav(){
@@ -48,34 +48,34 @@ export class AdminDashboardComponent implements OnInit {
   }
 
   ngOnInit() { 
-    if(window.innerWidth <= 520){
-      //Mobile device
-      require("style-loader!./../../../assets/admin_dashboard_styles/mobile_style.scss");
-      this.navWidth = "100%";
-    }
-    else if(window.innerWidth <= 1400 && window.innerWidth > 520){
-      //Tablet device
-      require("style-loader!./../../../assets/admin_dashboard_styles/tablet_style.scss");
+    // if(window.innerWidth <= 520){
+    //   //Mobile device
+    //   require("style-loader!./../../../assets/admin_dashboard_styles/mobile_style.scss");
+    //   this.navWidth = "100%";
+    // }
+    // else if(window.innerWidth <= 1400 && window.innerWidth > 520){
+    //   //Tablet device
+    //   require("style-loader!./../../../assets/admin_dashboard_styles/tablet_style.scss");
 
-      if(window.innerWidth > 920 && window.innerWidth < 1000){
-        this.navWidth = "50%";
-      }
-      else if(window.innerWidth >= 1000 && window.innerWidth < 1050){
-        this.navWidth = "40%";
-      }
-      else{
-        this.navWidth = "34%";
-      }
-    }
-    else if(window.innerWidth <= 2500 && window.innerWidth > 1400){
-      //Laptop device
-      require("style-loader!./admin-dashboard.component.scss");
-      this.navWidth = "22%";
-    }
-    else if(window.innerWidth > 2500){
-      //Desktop device
-      require("style-loader!./../../../assets/admin_dashboard_styles/desktop_style.scss");
-      this.navWidth = "70%";
-    }
+    //   if(window.innerWidth > 920 && window.innerWidth < 1000){
+    //     this.navWidth = "50%";
+    //   }
+    //   else if(window.innerWidth >= 1000 && window.innerWidth < 1050){
+    //     this.navWidth = "40%";
+    //   }
+    //   else{
+    //     this.navWidth = "34%";
+    //   }
+    // }
+    // else if(window.innerWidth <= 2500 && window.innerWidth > 1400){
+    //   //Laptop device
+    //   require("style-loader!./admin-dashboard.component.scss");
+    //   this.navWidth = "22%";
+    // }
+    // else if(window.innerWidth > 2500){
+    //   //Desktop device
+    //   require("style-loader!./../../../assets/admin_dashboard_styles/desktop_style.scss");
+    //   this.navWidth = "70%";
+    // }
   }
 }
