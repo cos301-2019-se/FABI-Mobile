b387e29a427927d0db08503566e713cf0c3dee67
Emma <u17074526@tuks.co.za>
2019-06-27 12:34:00 +0200

    Stats setup

--- FABI-Web/src/app/Admin/admin-dashboard/admin-dashboard.component.ts
+++ FABI-Web/src/app/Admin/admin-dashboard/admin-dashboard.component.ts
@@ -6,6 +6,18 @@ import { sharedStylesheetJitUrl } from '@angular/compiler';
 
 declare var require: any;
 
+//Object for defining how a member of FABI is structured
+export interface Member {
+  ID: string;       //This will contain the ID retreived from the DB 
+  Name: string;     //This will be the name of the member
+  Surname: string;  //This will be the surname of the member
+}
+
+//Object for defining how a sample is structured
+export interface Sample {
+  ID: string; //This will contain the ID retreived from the DB 
+}
+
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html'
@@ -14,7 +26,37 @@ declare var require: any;
 export class AdminDashboardComponent implements OnInit {
   navWidth: string;
 
+  /*
+    GLOBALS
+  */
+  members: Object;            //array containing all FABI members that are not admins
+  admins: Object;             //array containing all FABI members that are admins
+  samples: Object;            //array containing all current samples for FABI
+  completedSamples: Object;   //array containing all completed samples for FABI
+
   constructor(public sanitizer: DomSanitizer) { }
+
+  /*
+    This function will use an API service to get all the members of FABI. These members will be read into the
+    'members' Object. The function does not receive any parameters but it will populate a 'heading' element on the
+    HTML page with the number of members belonging to FABI. This function will also use API calls to populate
+    the admins object.
+  */
+  getNumberOfFABIMembers(){}
+
+  /*
+    This function will use an API service to get all the samples of FABI. These samples will be read into the
+    'samples' Object. The function does not receive any parameters but it will populate a 'heading' element on the
+    HTML page with the number of samples belonging to FABI.
+  */
+  getNumberOfFABISamples(){}
+
+  /*
+    This function will use an API service to get all the completed (processed) samples of FABI. These 
+    samples will be read into the 'completedSamples' Object. The function does not receive any parameters but it will 
+    populate a 'heading' element on the HTML page with the percentage of completed samples belonging to FABI.
+  */
+  getNumberOfCompletedFABISamples(){}
   
   sidenavToggle(){
     if(document.getElementById("sidenav_div").style.width == this.navWidth)
--- FABI-Web/src/app/Organization-Member/member-dashboard/member-dashboard.component.ts
+++ FABI-Web/src/app/Organization-Member/member-dashboard/member-dashboard.component.ts
@@ -1,13 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+// import { OrganizationAPIService } from '../organization-api.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material';
+import { MatDialog } from '@angular/material';
+// import { ErrorComponent } from '../error/error.component';
+import { Router } from '@angular/router';
+
+//Object for defining how a sample is structured
+export interface Sample {
+  ID: string; //This will contain the ID retreived from the DB 
+}
 
 @Component({
   selector: 'app-member-dashboard',
   templateUrl: './member-dashboard.component.html',
   styleUrls: ['./member-dashboard.component.scss']
 })
+
 export class MemberDashboardComponent implements OnInit {
 
+  /*
+    GLOBALS
+  */
+  samples: Object;            //array containing all current samples for the member
+  completedSamples: Object;   //array containing all completed samples for the member
+
   constructor() { }
+
+  /*
+    This function will use an API service to get all the samples of a member. These samples will be read into the
+    'samples' Object. The function does not receive any parameters but it will populate a 'heading' element on the
+    HTML page with the number of samples belonging to the member.
+  */
+  getNumberOfMemberSamples(){}
+
+  /*
+    This function will use an API service to get all the completed (processed) samples of a member. These 
+    samples will be read into the 'completedSamples' Object. The function does not receive any parameters but it will 
+    populate a 'heading' element on the HTML page with the percentage of completed samples belonging to the member.
+  */
+  getNumberOfCompletedMemberSamples(){}
 
   sidenavToggle(){
     if(document.getElementById("sidenav_div").style.width == "22%")
