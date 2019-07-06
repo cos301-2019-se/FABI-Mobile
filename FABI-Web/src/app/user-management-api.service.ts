95e478a4e014c34a7162209029dab665c665039a
Emma <u17074526@tuks.co.za>
2019-07-06 12:42:26 +0200

    Added statistics functionality from admin-dashboard

---
+++ FABI-Web/src/app/user-management-api.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { registerContentQuery } from '@angular/core/src/render3';
+import { StaticInjector } from '@angular/core/src/di/injector';
+import { BehaviorSubject } from 'rxjs';
+
+//Globals variables used to hold the API call urls
+const getAllFABIMembersURL = 'https://user-management-dot-api-fabi.appspot.com/getAllFabiMembers';
+const getAllFABIAdminsURL = 'https://user-management-dot-api-fabi.appspot.com/getAllFabiAdmins';
+
+//Object for defining how a member of FABI is structured
+export interface Member {
+    Email: string;      //This will contain the email retreived from the DB (is the unique identifier for the member) 
+    Name?: string;       //This will be the name of the member
+    Surname?: string;    //This will be the surname of the member
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class UserManagementAPIService {
+
+   constructor(private http: HttpClient) { }
+
+   /*
+        This function sends a POST request to the API to retrieve a list containing
+        all the FABI members
+   */
+    getAllFABIMembers() {
+        const options = {
+            method: 'POST',
+            url: getAllFABIMembersURL,
+            headers: {
+            'cache-control': 'no-cache',
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+            },
+            json: true
+        };
+
+        return this.http.request('POST', getAllFABIMembersURL, options);
+    }
+
+    /*
+        This function sends a POST request to the API to retrieve a list containing
+        all the FABI administrators
+   */
+    getAllFABIAdmins() {
+        const options = {
+            method: 'POST',
+            url: getAllFABIAdminsURL,
+            headers: {
+            'cache-control': 'no-cache',
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+            },
+            json: true
+        };
+
+        return this.http.request('POST', getAllFABIAdminsURL, options);        
+    }
+}
