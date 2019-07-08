a354c800ca47cad506269e027e4c7ce51972812a
Emma <u17074526@tuks.co.za>
2019-07-08 14:51:12 +0200

    Updated comments

--- FABI-Web/src/app/Staff/staff-dashboard/staff-dashboard.component.ts
+++ FABI-Web/src/app/Staff/staff-dashboard/staff-dashboard.component.ts
@@ -1,3 +1,18 @@
+/**
+ * File Name: staff-dashboard.component.ts
+ * File Path: c:\Users\Kendra\Documents\Varsity\Third Year\COS301\CAPSTONE\Git Repo\FABI-Mobile\FABI-Web\src\app\Staff\staff-dashboard\staff-dashboard.component.ts
+ * Project Name: fabi-web
+ * Created Date: Sunday, June 23rd 2019
+ * Author: Team Nova - novacapstone@gmail.com
+ * -----
+ * Last Modified: Monday, July 8th 2019
+ * Modified By: Team Nova
+ * -----
+ * Copyright (c) 2019 University of Pretoria
+ * 
+ * <<license>>
+ */
+
 import { Component, OnInit, ViewChild, ViewContainerRef, ComponentFactoryResolver } from '@angular/core';
 
 import { Member, UserManagementAPIService } from '../../services/user-management-api.service';
@@ -8,34 +23,43 @@ import { AdminDivComponent } from '../../Dynamic-Components/admin-div/admin-div.
   templateUrl: './staff-dashboard.component.html',
   styleUrls: ['./staff-dashboard.component.scss']
 })
+
 export class StaffDashboardComponent implements OnInit {
 
-  //Retriving an HTML element from the HTML page
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                                          GLOBAL VARIABLES
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  /** Holds the div element (adminContainer) from the HTML page - @type {ElementRef} */
   @ViewChild('adminContainer', {read: ViewContainerRef}) adminContainer;
 
-  admins: Member[] = [];         //array containing all FABI members that are admins
+  /** Object array for holding the administrators -  @type {Member[]} */
+  admins: Member[] = [];         
 
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                                             CONSTRUCTOR
+  /**
+   * Creates an instance of StaffDashboardComponent.
+   * 
+   * @param {UserManagementAPIService} userManagementService For calling the User Management API service
+   * @param {ComponentFactoryResolver} resolver For dynamically inserting elements into the HTML page
+   * @memberof StaffDashboardComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   constructor(private userManagementService: UserManagementAPIService, private resolver: ComponentFactoryResolver) { }
 
-  sidenavToggle(){
-    if(document.getElementById("sidenav_div").style.width == "22%")
-    {
-      document.getElementById("sidenav_div").style.width = "0";
-    }
-    else{
-      document.getElementById("sidenav_div").style.width = "22%";
-    } 
-  }
-
-  closeNav(){
-    document.getElementById("sidenav_div").style.width = "0";
-  }
-
-  /*
-  *  This function will load admin users into the section provided on the HTML page. 
-  *  This function will also dynamically load elements containing information about the administrators
-  *  to the HTML page dynamically
-  */
+ 
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                                LOAD_ADMINS
+  /**
+   *  This function will load admin users into the section provided on the HTML page. 
+   *  This function will also dynamically load elements containing information about the administrators
+   *  to the HTML page dynamically
+   * 
+   * @memberof StaffDashboardComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   loadAdmins(){
     //Subscribing to the UserManagementAPIService to get a list containing all the FABI members
     this.userManagementService.getAllFABIAdmins().subscribe((response: any) => {
@@ -61,17 +85,39 @@ export class StaffDashboardComponent implements OnInit {
     });
   }
 
-  /*
-  *  This function will load the staff member's notifications into the notification section on the HTML page
-  */
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                                  LOAD_NOTIFICATIONS
+  /**
+   *  This function will load the staff member's notifications into the notification section on the HTML page
+   * 
+   * @memberof StaffDashboardComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   loadNotifications(){}
 
-  /*
-  *  This function will remove a notification for the notification section when the user clicks on the 'exit'
-  *  button/icon associated with that notification
-  */
- removeNotification(){}
 
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                              REMOVE_NOTIFICATIONS
+  /**
+   *  This function will remove a notification for the notification section when the user clicks on the 'exit'
+   *  button/icon associated with that notification
+   * 
+   * @memberof StaffDashboardComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  removeNotification(){}
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                                    NG_ON_INIT()  
+  /**
+   * This function is called when the page loads
+   * 
+   * @description 1. Call loadAdmins() | 2. Call loadNotifications() 
+   * @memberof AdminDashboardComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   ngOnInit() {
     this.loadAdmins();
     this.loadNotifications();
