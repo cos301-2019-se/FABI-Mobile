348ee7a61d2eee4e5be5ba1f897e4ad22ae2821a
Emma <u17074526@tuks.co.za>
2019-07-08 15:03:03 +0200

    Updated comments

--- FABI-Web/src/app/services/user-management-api.service.ts
+++ FABI-Web/src/app/services/user-management-api.service.ts
@@ -1,9 +1,29 @@
+/**
+ * File Name: user-management-api.service.ts
+ * File Path: c:\Users\Kendra\Documents\Varsity\Third Year\COS301\CAPSTONE\Git Repo\FABI-Mobile\FABI-Web\src\app\services/user-management-api.service.ts
+ * Project Name: fabi-web
+ * Created Date: Saturday, Juky 6th 2019
+ * Author: Team Nova - novacapstone@gmail.com
+ * -----
+ * Last Modified: Monday, July 8th 2019
+ * Modified By: Team Nova
+ * -----
+ * Copyright (c) 2019 University of Pretoria
+ * 
+ * <<license>>
+ */
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { registerContentQuery } from '@angular/core/src/render3';
 import { StaticInjector } from '@angular/core/src/di/injector';
 import { BehaviorSubject } from 'rxjs';
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                          GLOBAL VARIABLES
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //Globals variables used to hold the API call urls
 const getAllFABIMembersURL = '***REMOVED***/getAllFabiMembers';
@@ -28,12 +48,28 @@ export interface POSTOrganization{
 
 export class UserManagementAPIService {
 
+   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+   //                                                             CONSTRUCTOR
+   /**
+   * Creates an instance of UserManagementAPIService.
+   * 
+   * @param {HttpClient} http For making calls to the API
+   * @memberof UserManagementAPIService
+   */
+   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
    constructor(private http: HttpClient) { }
 
-   /*
-    *    This function sends a POST request to the API to retrieve a list containing
-    *    all the FABI members
-    */
+
+   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                                     GET_ALL_FABI_MEMBERS 
+  /**
+   *    This function sends a POST request to the API to retrieve a list containing
+   *    all the FABI members
+   *
+   * @returns API response @type any
+   * @memberof UserManagementAPIService
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     getAllFABIMembers() {
         const options = {
             method: 'POST',
@@ -49,10 +85,17 @@ export class UserManagementAPIService {
         return this.http.request('POST', getAllFABIMembersURL, options);
     }
 
-    /*
-    *    This function sends a POST request to the API to retrieve a list containing
-    *    all the FABI administrators
-    */
+
+   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                                    GET_ALL_FABI_ADMINS 
+  /**
+   *    This function sends a POST request to the API to retrieve a list containing
+   *    all the FABI administrators
+   *
+   * @returns API response @type any
+   * @memberof UserManagementAPIService
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     getAllFABIAdmins() {
         const options = {
             method: 'POST',
@@ -68,10 +111,18 @@ export class UserManagementAPIService {
         return this.http.request('POST', getAllFABIAdminsURL, options);        
     }
 
-    /*
-    *    This function sends a POST request to the API to retrieve a list containing
-    *    all the Members of an Organization
-    */
+
+   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                                 GET_ALL_ORGANIZATION_MEMBERS 
+  /**
+   *    This function sends a POST request to the API to retrieve a list containing
+   *    all the Members of an Organization
+   *
+   * @returns API response @type any
+   * @param {string} organization Name of the organization
+   * @memberof UserManagementAPIService
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     getAllOrganizationMembers(organization: string) {
         var data: POSTOrganization = { orgName: organization };
         console.log(data);
