d6a6493183f361b42d032fec5e11a656e9b5f766
luveshanm <u17087709@tuks.co.za>
2019-07-14 00:34:14 +0200

    added code to the api to read and write logs

---
+++ API/Logging/api/routes/ERRL.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const router = express.Router();
+
+router.get("/", addLog );
+
+const db = admin.firestore();
+
+function addLog(req, res){
+	
+	if(req.body.Log.type == undefined || req.body.Log.type == '' ){
+		res.setHeader('Content-Type', 'application/problem+json');
+        res.setHeader('Content-Language', 'en');
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.status(400).json({                                  // ******* RESPONSE STATUS ************
+            success: false,
+            error: {
+                code: 400,
+                title: "BAD_REQUEST",
+                message: "Log type expected"
+            }
+        });
+		
+	}else if(req.body.Log.statusCode == undefined || req.body.Log.statusCode == '' ){
+		res.setHeader('Content-Type', 'application/problem+json');
+        res.setHeader('Content-Language', 'en');
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.status(400).json({                                  // ******* RESPONSE STATUS ************
+            success: false,
+            error: {
+                code: 400,
+                title: "BAD_REQUEST",
+                message: "Log statusCode expected"
+            }
+        });
+		
+	}else if(req.body.Log.details == undefined || req.body.Log.details == '' ){
+		res.setHeader('Content-Type', 'application/problem+json');
+        res.setHeader('Content-Language', 'en');
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.status(400).json({                                  // ******* RESPONSE STATUS ************
+            success: false,
+            error: {
+                code: 400,
+                title: "BAD_REQUEST",
+                message: "Log details expected"
+            }
+        });
+		
+	}else if(req.body.Log.user == undefined || req.body.Log.user == '' ){
+		res.setHeader('Content-Type', 'application/problem+json');
+        res.setHeader('Content-Language', 'en');
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.status(400).json({                                  // ******* RESPONSE STATUS ************
+            success: false,
+            error: {
+                code: 400,
+                title: "BAD_REQUEST",
+                message: "Log user expected"
+            }
+        });
+		
+	}else if(req.body.Log.type == "ERRL"){
+		
+		const logEntry = {
+			type: req.body.Log.type,
+			statusCode: req.body.Log.statusCode,
+			details: req.body.Log.details,
+			date: , //get on server side
+			user: req.body.Log.user
+		}
+		
+		var docRef  = db.collection('Logging').doc('ERRL').collection('logs').doc(logEntry.date);
+		docRef.set(logEntry).then(() => {
+			res.setHeader('Content-Type', 'application/problem+json');
+			res.setHeader('Content-Language', 'en');
+			res.setHeader("Access-Control-Allow-Origin", "*");
+			res.status(200).json({                                  // ******* RESPONSE STATUS ************
+				success: true,
+				data: {
+					code: 200,
+					title: "SUCCESS",
+					message: "Error log created",
+				}
+			});
+		});	
+		
+		
+	}else{
+		res.setHeader('Content-Type', 'application/problem+json');
+        res.setHeader('Content-Language', 'en');
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.status(400).json({
+            success: false,
+            error: {
+                code: 400,
+                title: "BAD_REQUEST",
+                message: "Did not recognise Log"
+            }
+        });
+	}
+	
+	
+}
+
+module.exports = router;
