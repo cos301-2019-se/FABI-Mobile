6976e4d423a2c0f08e307d33d26c4f9658f15967
luveshanm <u17087709@tuks.co.za>
2019-07-16 17:19:23 +0200

    added services to get logs

---
+++ API/Logging/api/routes/sendlog.js
@@ -0,0 +1,25 @@
+module.exports = function(log){
+	
+	if(log.type == "ACCL"){
+		
+		var qs = {
+			Log: {
+				type: log.type,
+				statusCode: log.statusCode,
+				
+		
+	}else if(log.type == "ERRL" ){
+		
+		
+	}else if(log.type == "USER" ){
+		
+		
+	}else if(log.type == "DBML" ){
+		
+		
+	}else if(log.type == "DGCL" ){
+		
+		
+	}		
+
+}
