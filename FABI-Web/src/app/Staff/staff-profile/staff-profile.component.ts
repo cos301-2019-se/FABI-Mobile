394c82e56d8f79f14d5fb596240e32646f0069aa
Emma <u17074526@tuks.co.za>
2019-07-18 16:11:49 +0200

    Staff and Admin profile updates working

--- FABI-Web/src/app/Staff/staff-profile/staff-profile.component.ts
+++ FABI-Web/src/app/Staff/staff-profile/staff-profile.component.ts
@@ -46,6 +46,18 @@ export class StaffProfileComponent implements OnInit {
   /** The form to display the staff member's details -  @type {FormGroup} */
   staffProfileForm: FormGroup;
 
+  /** Object array for holding all of the logs -  @type {any[]} */ 
+  allNotifications: any[] = [];
+  /** Object array for holding all of the read logs -  @type {any[]} */ 
+  readNotifications: any[] = [];
+
+  /** Indicates if there are notifications to load - @type {boolean} */           
+  notifications: boolean = true; 
+  /** The total number of User Logs - @type {number} */           
+  numberOfUserLogs: number = 0;
+  /** THe number of the notifications - @type {number} */   
+  localNotificationNumber : number = 1;
+
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //                                                             CONSTRUCTOR
   /**
@@ -54,18 +66,94 @@ export class StaffProfileComponent implements OnInit {
    * @param {UserManagementAPIService} userManagementService For calling the User Management API service
    * @param {NotificationLoggingService} notificationLoggingService For calling the Notification Logging API service
    * @param {MatSnackBar} snackBar For snack-bar pop-up messages
-   * @memberof AdminProfileComponent
+   * @memberof StaffProfileComponent
    */
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   constructor(private userManagementService: UserManagementAPIService, private formBuilder: FormBuilder, 
     private notificationLoggingService: NotificationLoggingService, private snackBar: MatSnackBar) { 
     this.staffProfileForm = this.formBuilder.group({
       organization_name: '',
-      staff_name: ['', Validators.required],
-      staff_surname: ['', Validators.required],
-      staff_email: ['', Validators.required],
+      staff_name: '',
+      staff_surname: '',
+      staff_email: '',
       staff_type: ''
     });
+  }
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                                  LOAD_NOTIFICATIONS
+  /**
+   *  This function will load the staff member's notifications into the notification section on the HTML page
+   * 
+   * @memberof StaffProfileComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  loadNotifications(){
+    //Need to fetch all notifications from local storage to make sure that notifications that have been read are not reloaded
+    const storageNotifications = JSON.parse(localStorage.getItem('readNotifications'));
+
+    //Loading the 'USER' logs
+    this.notificationLoggingService.getAllUserLogs().subscribe((response: any) => {
+      if(response.success = true){
+        const data = response.data.content.data.Logs;
+
+        for(var i = 0; i < data.length; i++){
+          var tempLog: UserLogs = {Type: 'USER', Action: data[i].action, Date: data[i].date, Details: data[i].details, User: data[i].user, Organization1: data[i].org1, Organization2: data[i].org2, MoreInfo: data[i].moreInfo, ID: this.localNotificationNumber};
+          
+          if(storageNotifications != null && storageNotifications.length != 0){
+            for(var j = 0; j < storageNotifications.length; j++){
+              if(storageNotifications[j].Type == 'USER' && storageNotifications[i].Action == tempLog.Action && 
+                 storageNotifications[i].Date == tempLog.Date && storageNotifications.User == tempLog.User){
+                this.readNotifications.push(tempLog);
+              }
+              else{
+                const user1 = this.loadUserDetails(tempLog.Organization2, tempLog.Details);
+                const user2 = this.loadUserDetails(tempLog.Organization1, tempLog.User);
+
+                if(tempLog.Action == 'C'){
+                  tempLog.Action = user1 + ' was added to the system by ' + user2;
+                }
+                else if(tempLog.Action == 'D'){
+                  tempLog.Action = user1 + ' was removed from the system by ' + user2;
+                }
+                else if(tempLog.Action == 'U'){
+                  tempLog.Action = user1 + ' details where updated by ' + user2;
+                }
+
+                this.allNotifications.push(tempLog);
+                this.numberOfUserLogs += 1;
+                this.localNotificationNumber += 1;
+              }
+            }
+          }
+          else{
+            const user1 = this.loadUserDetails(tempLog.Organization2, tempLog.Details);
+            const user2 = this.loadUserDetails(tempLog.Organization1, tempLog.User);
+
+            if(tempLog.Action == 'C'){
+              tempLog.Action = user1 + ' was added to the system by ' + user2;
+            }
+            else if(tempLog.Action == 'D'){
+              tempLog.Action = user1 + ' was removed from the system by ' + user2;
+            }
+            else if(tempLog.Action == 'U'){
+              tempLog.Action = user1 + ' details where updated by ' + user2;
+            }
+
+            this.allNotifications.push(tempLog);
+            this.numberOfUserLogs += 1;
+            this.localNotificationNumber += 1;
+          }
+        }
+      }
+      else{
+        //Error handling
+      }
+    });
+
+    //Pushing the readNotifications array to local storage
+    localStorage.setItem('readNotifications', JSON.stringify(this.readNotifications));
   }
 
 
@@ -74,7 +162,7 @@ export class StaffProfileComponent implements OnInit {
   /**
    *  This function will use an API service to load all the staff member's details into the elements on the HTML page.
    * 
-   * @memberof AdminProfileComponent
+   * @memberof StaffProfileComponent
    */
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   loadStaffProfileDetails(){
@@ -97,27 +185,19 @@ export class StaffProfileComponent implements OnInit {
     });
   }
 
-
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //                                                  LOAD_USER_DETAILS
   /**
    *  This function will be called so that the information of a specific user can be fetched
-   *  @memberof StaffProfileComponent
+   *  @memberof ReportingComponent
    */
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  loadUserDetails() {
-    this.email = localStorage.getItem('email');
-    this.organization = localStorage.getItem('organization');
-
-    this.userManagementService.getAllFABIMembers().subscribe((response: any) => {
+  loadUserDetails(userOrganization: string, userID: string) {
+    this.userManagementService.getUserDetails(userOrganization, userID).subscribe((response: any) => {
       if(response.success == true){
-        var tempStaff = response.data.qs.staff;
-        for(var i = 0; i < tempStaff.length; i++){
-          if(tempStaff[i].email == this.email){
-            this.name = tempStaff[i].fname;
-            this.surname = tempStaff[i].surname;
-          }
-        }
+        const data = response.data;
+
+        return data.fname + ' ' + data.surname;
       } 
       else{
         //Error control
@@ -134,13 +214,26 @@ export class StaffProfileComponent implements OnInit {
    */
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   saveChanges(){
-    if (this.staffProfileForm.invalid) {
-      return;
+    if(this.staffProfileForm.controls.admin_email.value == ''){
+      this.email = this.email;
+    }
+    else{
+      this.email = this.staffProfileForm.controls.admin_email.value;
     }
 
-    this.email = this.staffProfileForm.controls.admin_email.value;
-    this.name = this.staffProfileForm.controls.admin_name.value;
-    this.surname = this.staffProfileForm.controls.admin_surname.value;
+    if(this.staffProfileForm.controls.admin_name.value == ''){
+      this.name = this.name;
+    }
+    else{
+      this.name = this.staffProfileForm.controls.admin_name.value;
+    }
+
+    if(this.staffProfileForm.controls.admin_surname.value == ''){
+      this.surname == this.surname;
+    }
+    else{
+      this.surname = this.staffProfileForm.controls.admin_surname.value;
+    }
 
     this.userManagementService.updateFABIMemberDetails(this.email, this.name, this.surname, this.id).subscribe((response: any) => {
       if(response.success == true){
@@ -161,8 +254,8 @@ export class StaffProfileComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.loadUserDetails();
     this.loadStaffProfileDetails();
+    this.loadNotifications();
   }
 
 }
