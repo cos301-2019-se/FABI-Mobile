afe5f674ae96677a07efa06a8a3ff5765fe6f44a
aeronland <aeronland@gmail.com>
2019-07-19 00:38:45 +0200

    ''

--- API/Logging/app.js
+++ API/Logging/app.js
@@ -10,6 +10,7 @@ admin.initializeApp({
 
 const displayHTML = require('./api/routes/displayHTML');
 
+<<<<<<< HEAD:API/Logging/app.js
 //log modules
 const loggingUSER = require('./api/routes/USER');
 const loggingDBML = require('./api/routes/DBML');
@@ -17,6 +18,13 @@ const loggingDGCL = require('./api/routes/DGCL');
 const loggingACCL = require('./api/routes/ACCL');
 const loggingERRL = require('./api/routes/ERRL');
 const getLogs = require('./api/routes/getLogs');
+=======
+//fabi modules
+const createDatabaseRoute = require('./api/routes/createDatabase');
+const addDocRoute = require('./api/routes/addDoc');
+const portingRoute = require('./api/routes/porting');
+const retrieveDatabaseRoute = require('./api/routes/retrieveDatabase');
+>>>>>>> remotes/origin/API:API/DatabaseManagement/app.js
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -25,12 +33,20 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/',displayHTML);
+<<<<<<< HEAD:API/Logging/app.js
 app.use('/USER', loggingUSER);
 app.use('/DBML', loggingDBML);
 app.use('/DGCL', loggingDGCL);
 app.use('/ACCL', loggingACCL);
 app.use('/ERRL', loggingERRL);
 app.use('/getLogs', getLogs);
+=======
+app.use('/createDatabase', createDatabaseRoute);
+app.use('/addDoc', addDocRoute);
+app.use('/porting', portingRoute);
+app.use('/retrieveDatabase', retrieveDatabaseRoute);
+
+>>>>>>> remotes/origin/API:API/DatabaseManagement/app.js
 
 //Error handling when url doesn't exist
 // app.use((req, res, next) => {
