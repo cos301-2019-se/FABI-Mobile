c81cc1ae4667368c91839a79d257807af92f3e72
Kendra <kendrar@mweb.co.za>
2019-07-18 19:11:54 +0200

    In App Navigation

---
+++ FABI-Web/src/app/_interfaces/role.ts
@@ -0,0 +1,23 @@
+/**
+ * File Name: role.ts
+ * File Path: c:\Users\Kendra\Documents\Varsity\Third Year\COS301\CAPSTONE\Git Repo\FABI-Mobile\FABI-Web\src\app\_interfaces\role.ts
+ * Project Name: fabi-web
+ * Created Date: Monday, July 15th 2019
+ * Author: Team Nova - novacapstone@gmail.com
+ * -----
+ * Last Modified: Wednesday, July 17th 2019
+ * Modified By: Team Nova
+ * -----
+ * Copyright (c) 2019 University of Pretoria
+ * 
+ * <<license>>
+ */
+
+
+export enum Role {
+    SuperUser = 'superUser',
+    ClinicAdmin = 'ClinicAdmin',
+    Staff = 'Staff',
+    Organisation = 'Organization',
+    Member = 'Member'
+}
