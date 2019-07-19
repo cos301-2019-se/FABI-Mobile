22c76d718d5532ad3f5b6310412605589122bfd6
Kendra <kendrar@mweb.co.za>
2019-07-19 02:28:39 +0200

    Security

--- FABI-Web/src/app/Organization-Member/organization-member-routing.module.ts
+++ FABI-Web/src/app/Organization-Member/organization-member-routing.module.ts
@@ -5,7 +5,7 @@
  * Created Date: Friday, May 24th 2019
  * Author: Team Nova - novacapstone@gmail.com
  * -----
- * Last Modified: Thursday, July 18th 2019
+ * Last Modified: Friday, July 19th 2019
  * Modified By: Team Nova
  * -----
  * Copyright (c) 2019 University of Pretoria
