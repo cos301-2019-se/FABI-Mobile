3984faf379760e706bf2df9e7a3bb9b207d73e3a
Emma <u17074526@tuks.co.za>
2019-07-23 08:58:28 +0200

    Adding CBS deposit form functionality

--- FABI-Web/src/app/Staff/submit-cmw-deposit/submit-cmw-deposit.component.ts
+++ FABI-Web/src/app/Staff/submit-cmw-deposit/submit-cmw-deposit.component.ts
@@ -1,4 +1,23 @@
+/**
+ * File Name: submit-cmw-deposit.component.ts
+ * File Path: c:\Users\Kendra\Documents\Varsity\Third Year\COS301\CAPSTONE\Git Repo\FABI-Mobile\FABI-Web\src\app\Staff\submit-cmw-deposit\submit-cmw-deposit.component.ts
+ * Project Name: fabi-web
+ * Created Date: Tuesday, July 16th 2019
+ * Author: Team Nova - novacapstone@gmail.com
+ * -----
+ * Last Modified: Tuesday, July 23rd 2019
+ * Modified By: Team Nova
+ * -----
+ * Copyright (c) 2019 University of Pretoria
+ * 
+ * <<license>>
+ */
+
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { UserManagementAPIService, Member } from '../../_services/user-management-api.service';
 
 @Component({
   selector: 'app-submit-cmw-deposit',
@@ -7,9 +26,109 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubmitCmwDepositComponent implements OnInit {
 
-  constructor() { }
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                                          GLOBAL VARIABLES
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  /** The form to submit a CMW deposit form -  @type {FormGroup} */
+  cmwDepositForm: FormGroup;
+
+  /** Object array for holding the staff members -  @type {string[]} */                        
+  staff: string[] = []; 
+  /** Object array for holding the staff members -  @type {String[]} */
+  filteredOptions: Observable<string[]>;
+  control = new FormControl();
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                                             CONSTRUCTOR
+  /**
+   * Creates an instance of SubmitCmwDepositComponent.
+   * 
+   * @param {userManagementService} userManagementService For making calls to the User Management API Service
+   * @param {MatSnackBar} snackBar For snack-bar pop-up messages
+   * @memberof SubmitCmwDepositComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  constructor(private formBuilder: FormBuilder, private userManagementService: UserManagementAPIService) { 
+    this.cmwDepositForm = this.formBuilder.group({
+      cmw_culture_number: '',
+      genus: '',
+      epitheton: '',
+      personal_collection_number: '',
+      international_collection_number: '',
+      herbarium_number: '',
+      other_FABI_collections: '',
+      name: '',
+      type_status: '',
+      host: '',
+      vector: '',
+      substrate: '',
+      continent: '',
+      country: '',
+      region: '',
+      locality: '',
+      gps: '',
+      collected_by: '',
+      date_collected: null,
+      isolated_by: '',
+      identified_by: '',
+      donated_by: '',
+      additional_notes: ''
+    });
+  }
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                              SUBMIT_CMW_DEPOSIT_FORM
+  /**
+   * This function will submit a CMW Deposit form based on the information provided in the form on the HTML page.
+   * @memberof SubmitCmwDepositComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  submitCMWDepositForm(){
+
+  }
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                            GET_ALL_STAFF
+  /**
+   *  This function will load of the FABI staff members and administrators into an array so that they can be selected when filling out
+   *  the form to submit.
+   * @memberof SubmitCmwDepositComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  getAllStaff(){
+    //Subscribing to the UserManagementAPIService to get a list containing all the FABI members
+    this.userManagementService.getAllFABIMembers().subscribe((response: any) => {
+      if(response.success == true){
+        
+        for(var i = 0; i <= response.data.qs.admins.length; i++){
+          this.staff.push(response.data.qs.admins[i].surname + ', ' + response.data.qs.admins[i].fname[0]);
+        }
+
+        for(var i = 0; i <= response.data.qs.admins.length; i++){
+          this.staff.push(response.data.qs.admins[i].surname + ', ' + response.data.qs.admins[i].fname[0]);
+        }
+      }
+    });
+  }
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                            FILTER
+  /**
+   *  This function will filter the autocomplete results on the form.
+   * @memberof SubmitCmwDepositComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  private filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.staff.filter(option => option.toLowerCase().includes(filterValue));
+  }
 
   ngOnInit() {
+    this.filteredOptions = this.control.valueChanges.pipe(startWith(''), map(value => this.filter(value)));
   }
 
 }
