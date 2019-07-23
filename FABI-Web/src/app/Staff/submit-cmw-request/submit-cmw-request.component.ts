67aa37ccc2f5528bfb9553a3fa6036b572b14b55
Emma <u17074526@tuks.co.za>
2019-07-23 15:37:37 +0200

    Updated CMW request form

--- FABI-Web/src/app/Staff/submit-cmw-request/submit-cmw-request.component.ts
+++ FABI-Web/src/app/Staff/submit-cmw-request/submit-cmw-request.component.ts
@@ -1,4 +1,24 @@
+/**
+ * File Name: submit-cmw-request.component.ts
+ * File Path: c:\Users\Kendra\Documents\Varsity\Third Year\COS301\CAPSTONE\Git Repo\FABI-Mobile\FABI-Web\src\app\Staff\submit-cmw-request\submit-cmw-request.component.ts
+ * Project Name: fabi-web
+ * Created Date: Tuesday, July 16th 2019
+ * Author: Team Nova - novacapstone@gmail.com
+ * -----
+ * Last Modified: Tuesday, July 23rd 2019
+ * Modified By: Team Nova
+ * -----
+ * Copyright (c) 2019 University of Pretoria
+ * 
+ * <<license>>
+ */
+
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { UserManagementAPIService, Member } from '../../_services/user-management-api.service';
+import { DiagnosticClinicAPIService, CMWDeposit } from '../../_services/diagnostic-clinic-api.service';
 
 @Component({
   selector: 'app-submit-cmw-request',
@@ -7,9 +27,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubmitCmwRequestComponent implements OnInit {
 
-  constructor() { }
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                                          GLOBAL VARIABLES
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  /** The form to submit a CMW request form -  @type {FormGroup} */
+  cmwRequestForm: FormGroup;
+
+  /** Object array for holding the staff members -  @type {string[]} */                        
+  staff: string[] = []; 
+  /** Object array for holding the staff members -  @type {String[]} */
+  filteredOptions: Observable<string[]>;
+  control = new FormControl();
+
+  
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                                             CONSTRUCTOR
+  /**
+   * Creates an instance of SubmitCmwDepositComponent.
+   * 
+   * @param {UserManagementAPIService} userManagementService For making calls to the User Management API Service
+   * @param {DiagnosticClinicAPIService} diagnosticClinicService for making calls to the Diagnostic Clinic API Service
+   * @param {MatSnackBar} snackBar For snack-bar pop-up messages
+   * @memberof SubmitCmwDepositComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  constructor(private formBuilder: FormBuilder, private userManagementService: UserManagementAPIService,
+    private diagnosticClinicService: DiagnosticClinicAPIService) { 
+    this.cmwRequestForm = this.formBuilder.group({
+      requestor: '',
+      taxon_name: '',
+      culture_number: '',
+      date_requested: null,
+      reference_number: '',
+      notes: '',
+    });
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                              SUBMIT_CMW_REQUEST_FORM
+  /**
+   * This function will submit a CMW Request form based on the information provided in the form on the HTML page.
+   * @memberof SubmitCmwDepositComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  submitCMWRequestForm(){
+
+  }
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                            GET_ALL_STAFF
+  /**
+   *  This function will load of the FABI staff members and administrators into an array so that they can be selected when filling out
+   *  the form to submit.
+   * @memberof SubmitCmwDepositComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  getAllStaff(){
+    //Subscribing to the UserManagementAPIService to get a list containing all the FABI members
+    this.userManagementService.getAllFABIMembers().subscribe((response: any) => {
+      if(response.success == true){
+        
+        for(var i = 0; i <= response.data.qs.admins.length; i++){
+          this.staff.push(response.data.qs.admins[i].surname + ', ' + response.data.qs.admins[i].fname[0]);
+        }
+
+        for(var i = 0; i <= response.data.qs.admins.length; i++){
+          this.staff.push(response.data.qs.admins[i].surname + ', ' + response.data.qs.admins[i].fname[0]);
+        }
+      }
+    });
+  }
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                            FILTER
+  /**
+   *  This function will filter the autocomplete results on the form.
+   * @memberof SubmitCmwDepositComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  private filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.staff.filter(option => option.toLowerCase().includes(filterValue));
+  }
 
   ngOnInit() {
+    this.filteredOptions = this.control.valueChanges.pipe(startWith(''), map(value => this.filter(value)));
   }
 
 }
