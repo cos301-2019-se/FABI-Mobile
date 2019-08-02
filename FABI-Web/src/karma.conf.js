c773cd56e99d34033a390bd6966b61c934d0c006
luveshanm <u17087709@tuks.co.za>
2019-08-02 23:22:26 +0200

    updated karma config

--- FABI-Web/src/karma.conf.js
+++ FABI-Web/src/karma.conf.js
@@ -26,6 +26,14 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+	browserDisconnectTimeout: 10000,
+    browserDisconnectTolerance: 3,
+    browserNoActivityTimeout: 60000,
+    flags: [
+      '--disable-web-security',
+      '--disable-gpu',
+      '--no-sandbox'
+    ],
     singleRun: false,
     restartOnFileChange: true
   });
