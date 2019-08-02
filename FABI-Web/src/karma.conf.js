3cf6cbba111f5f3dab8fd94eb11e798ffaba3c12
Luveshan Marimuthu <47663519+Luveshanm@users.noreply.github.com>
2019-08-02 23:30:11 +0200

    Update Karma config

--- FABI-Web/src/karma.conf.js
+++ FABI-Web/src/karma.conf.js
@@ -24,7 +24,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: ['Chrome'],
 	browserDisconnectTimeout: 10000,
     browserDisconnectTolerance: 3,
