9e58649dbb464695c3e1aedc798df96ae59ce950
Luveshan Marimuthu <47663519+Luveshanm@users.noreply.github.com>
2019-08-02 23:33:43 +0200

    change karma to run on firefox

--- FABI-Web/src/karma.conf.js
+++ FABI-Web/src/karma.conf.js
@@ -24,16 +24,8 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: ['Chrome'],
-	browserDisconnectTimeout: 10000,
-    browserDisconnectTolerance: 3,
-    browserNoActivityTimeout: 60000,
-    flags: [
-      '--disable-web-security',
-      '--disable-gpu',
-      '--no-sandbox'
-    ],
+    autoWatch: true,
+    browsers: ['Firefox'],
     singleRun: false,
     restartOnFileChange: true
   });
