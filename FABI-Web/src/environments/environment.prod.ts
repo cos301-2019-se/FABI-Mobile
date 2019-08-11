f3dfbc30f970377b734fc0dbea66c5dd1f6f0e30
Kendra <kendrar@mweb.co.za>
2019-08-11 18:24:13 +0200

    Temporary Services

--- FABI-Web/src/environments/environment.prod.ts
+++ FABI-Web/src/environments/environment.prod.ts
@@ -3,11 +3,12 @@ export const environment = {
 };
 
 export const config = {
-  loginURL: "***REMOVED***",
-  databaseManagementURL: "***REMOVED***",
-  userManagementURL: "***REMOVED***",
-  cultureClinicURL: "***REMOVED***",
-  logsURL: "***REMOVED***",
-  APIKEy: '***REMOVED***',
-  diagnosticClinicURL: "***REMOVED***",
+  loginURL: "https://login-dot-api-fabi.appspot.com",
+  databaseManagementURL: "https://database-management-dot-api-fabi.appspot.com",
+  userManagementURL: "https://user-management-dot-api-fabi.appspot.com",
+  cultureClinicURL: "https://culture-collection-management-dot-api-fabi.appspot.com",
+  logsURL: "https://logging-dot-api-fabi.appspot.com",
+  diagnosticClinicURL: "https://diagnostic-clinic-dot-api-fabi.appspot.com",
+  
+  APIKEy: 'AIzaSyDsHFsV7YDWrjGHOIfEkS8y2M738quo0tk'
 }
