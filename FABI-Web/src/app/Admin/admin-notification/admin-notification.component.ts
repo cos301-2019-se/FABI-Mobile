ba3fd8c8c5496b750166122d9fb766dfceb70254
Kendra <kendrar@mweb.co.za>
2019-08-13 13:15:46 +0200

    Notification Components Update

--- FABI-Web/src/app/Admin/admin-notification/admin-notification.component.ts
+++ FABI-Web/src/app/Admin/admin-notification/admin-notification.component.ts
@@ -1,4 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+/**
+ * File Name: admin-notification.component.ts
+ * File Path: c:\Users\Kendra\Documents\Varsity\Third Year\COS301\CAPSTONE\Git Repo\NEW\FABI-Mobile\FABI-Web\src\app\Admin\admin-notification\admin-notification.component.ts
+ * Project Name: fabi-web
+ * Created Date: Tuesday, August 13th 2019
+ * Author: Team Nova - novacapstone@gmail.com
+ * -----
+ * Last Modified: Tuesday, August 13th 2019
+ * Modified By: Team Nova
+ * -----
+ * Copyright (c) 2019 University of Pretoria
+ * 
+ * <<license>>
+ */
+
+
+import { Component, ViewChild, ElementRef, isDevMode, Inject, Output, EventEmitter, TemplateRef,
+  ComponentFactory, ComponentRef, ComponentFactoryResolver, ViewContainerRef, ChangeDetectorRef} from '@angular/core';
+import { OnInit} from '@angular/core';
+import { Injectable } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { DomSanitizer } from '@angular/platform-browser';
+import { sharedStylesheetJitUrl } from '@angular/compiler';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
+
+import { Member, UserManagementAPIService } from '../../_services/user-management-api.service';
+import { DiagnosticClinicAPIService } from '../../_services/diagnostic-clinic-api.service';
+import { NotificationLoggingService, UserLogs, DatabaseManagementLogs, AccessLogs } from '../../_services/notification-logging.service';
+import { AuthenticationService } from 'src/app/_services/authentication.service';
+
+import * as Interface from '../../_interfaces/interfaces';
 
 @Component({
   selector: 'app-admin-notification',
@@ -7,9 +39,370 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminNotificationComponent implements OnInit {
 
-  constructor() { }
+  /** Contains the user stats that will be dynamically loaded in the HTML page - @type {string} */
+  userStats: string;
+  /** Contains the sample stats that will be dynamically loaded in the HTML page - @type {string} */
+  sampleStats: string;
+
+  /** Object array for holding the administrators -  @type {Member[]} */
+  admins: Member[] = []; 
+  /** Object array for holding the staff members -  @type {Member[]} */                        
+  staff: Member[] = [];  
+  /** Object array for holding the database administrators -  @type {Member[]} */                        
+  databaseAdmins: Member[] = [];   
+  /** Object array for holding the culture curators -  @type {Member[]} */              
+  cultureCurators: Member[] = []; 
+  /** Object array for holding the diagnostic clinic administrators -  @type {Member[]} */               
+  diagnosticClinicAdmins: Member[] = []; 
+  /** Object array for holding all of FABI's samples -  @type {Object[]} */        
+  samples: Object[] = [];  
+  /** Object array for holding all of FABI's completed samples -  @type {Object[]} */                      
+  completedSamples: Object[] = [];  
+
+  /** Object array for holding all of the logs -  @type {any[]} */ 
+  allNotifications: any[] = [];
+  /** Object array for holding all of the logs that have not been read -  @type {any[]} */ 
+  newNotifications: any[] = [];
+  /** Object array for holding all of the logs that have not been read -  @type {string[]} */ 
+  allLogs: string[] = [];
+
+  /** The total number of User Logs - @type {number} */           
+  numberOfUserLogs: number = 0;
+  /** The total number of Database Management Logs - @type {number} */           
+  numberOfDatabaseLogs: number = 0;
+  /** The total number of Access Logs - @type {number} */           
+  numberOfAccessLogs: number = 0;
+
+  /** The total number of FABI staff members - @type {number} */
+  numberOfFABIMembers: number;        
+  /** The total number of FABI samples - @type {number} */           
+  numberOfSamples: number;
+  /** Indicates if there are notifications to load - @type {boolean} */           
+  notifications: boolean = true; 
+  /** The number of the notifications - @type {number} */   
+  localNotificationNumber : number = 1;    
+  
+  /** Indicates if the notifications tab is hidden/shown - @type {boolean} */   
+  notificationsTab: boolean = false;
+  /** Indicates if the profile tab is hidden/shown - @type {boolean} */  
+  profileTab: boolean = false;
+  /** Indicates if the save button is hidden/shown on the profile tab- @type {boolean} */  
+  saveBtn: boolean = false;
+  /** Indicates if the confirm password tab is hidden/shown on the profile tab - @type {boolean} */  
+  confirmPasswordInput: boolean = false;
+  /** Indicates if the help tab is hidden/shown - @type {boolean} */  
+  helpTab: boolean = false;
+
+  /** The name and surname of a user concatenated as a string - @type {string} */   
+  user1: string;
+  /** The name and surname of a user concatenated as a string - @type {string} */   
+  user2: string;
+
+  /** The staff member's email address -  @type {string} */
+  email: string = '';
+  /** The staff member's organization -  @type {string} */
+  organization: string = '';
+  /** The staff member's id -  @type {string} */
+  id: string = '';
+  /** The staff member's name -  @type {string} */
+  name: string = '';
+  /** The staff member's surname -  @type {string} */
+  surname: string = '';
+  /** The staff member's user type -  @type {string} */
+  userType: string = '';
+  /** The staff member's password -  @type {string} */
+  password: string = '';
+  /** The staff member's confirmed password -  @type {string} */
+  confirmPassword: string = '';
+
+  /** The form to display the admin member's details -  @type {FormGroup} */
+  adminProfileForm: FormGroup;
+
+  currentUser: any;
+
+  constructor(
+    public sanitizer: DomSanitizer, 
+    private userManagementService: UserManagementAPIService,
+    private diagnosticClinicService: DiagnosticClinicAPIService, 
+    private notificationLoggingService: NotificationLoggingService, 
+    private resolver: ComponentFactoryResolver, 
+    private authService: AuthenticationService, 
+    private router: Router,
+    private formBuilder: FormBuilder, 
+    private snackBar: MatSnackBar, 
+  ) { 
+    
+  }
 
   ngOnInit() {
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                                        GET DATE
+  /**
+   *  This function will put the string date provided into a more readable format for the notifications
+   * @param {string} date The date of the log
+   * 
+   * @memberof AdminNotificationComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  getDate(date: string){
+    var tempDate = (date).split(' ');
+    var newDate = '';
+
+    newDate += tempDate[2];
+
+    if(tempDate[0] == 'Mon'){
+      newDate += ' Monday ';
+    }
+    else if(tempDate[0] == 'Tue' || tempDate[0] == 'Tu' || tempDate[0] == 'Tues'){
+      newDate += ' Tuesday ';
+    }
+    else if(tempDate[0] == 'Wed'){
+      newDate += ' Wednesday ';
+    }
+    else if(tempDate[0] == 'Thu' || tempDate[0] == 'Thur' || tempDate[0] == 'Thurs'){
+      newDate += ' Thursday ';
+    }
+    else if(tempDate[0] == 'Fri'){
+      newDate += ' Friday ';
+    }
+    else if(tempDate[0] == 'Sat'){
+      newDate += ' Saturday ';
+    }
+    else if(tempDate[0] == 'Sun'){
+      newDate += ' Sunday ';
+    }
+
+    if(tempDate[1] == 'Jan'){
+      newDate += 'January';
+    }
+    else if(tempDate[1] == 'Feb'){
+      newDate += 'February';
+    }
+    else if(tempDate[1] == 'Mar'){
+      newDate += 'March';
+    }
+    else if(tempDate[1] == 'Apr'){
+      newDate += 'April';
+    }
+    else if(tempDate[1] == 'Jun'){
+      newDate += 'June';
+    }
+    else if(tempDate[1] == 'Jul'){
+      newDate += 'July';
+    }
+    else if(tempDate[1] == 'Aug'){
+      newDate += 'August';
+    }
+    else if(tempDate[1] == 'Sep' || tempDate[1] == 'Sept'){
+      newDate += 'September';
+    }
+    else if(tempDate[1] == 'Oct'){
+      newDate += 'October';
+    }
+    else if(tempDate[1] == 'Nov'){
+      newDate += 'November';
+    }
+    else if(tempDate[1] == 'Dec'){
+      newDate += 'December';
+    }
+
+    newDate += ' ' + tempDate[3];
+
+    return newDate;
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                                           LOAD LOGS
+  /**
+   *  This function will load all of the user's logs into a string array.
+   * 
+   * @memberof AdminNotificationComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  loadLogs(){
+    //Getting the user's details from local storage
+    var tempUser = this.authService.getCurrentSessionValue();
+
+    //Making a call to the notification logging service to return all logs belonging to the user
+    this.notificationLoggingService.getUserLogs(tempUser.user.ID).subscribe((response: any) => {
+      if(response.success == true){
+        var data = response.data.content.data.Logs;
+
+        for(var i = 0; i < data.length; i++){
+          this.allLogs.push(data[i].id);
+        }
+      }
+      else{
+        //Error handling
+      }
+    });
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                                       LOAD NOTIFICATIONS
+  /**
+   *  This function will load the admin's notifications into the notification section on the HTML page
+   * 
+   * @memberof AdminNotificationComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  loadNotifications(){
+    //Loading all the logs beloning to the user
+    this.loadLogs();
+
+    //Making a call too the notification logging service to return all USER logs
+    this.notificationLoggingService.getAllUserLogs().subscribe((response: any) => {
+      if(response.success = true){
+        //Temporarily holds the data returned from the API call
+        const data = response.data.content.data.Logs;
+
+        for(var i = 0; i < data.length; i++){
+          for(var j = 0; j < this.allLogs.length; j++){
+            if(data[i].date == this.allLogs[j]){
+              //A temporary instance of UserLogs that will be added to the allNotifications array
+              var tempLogU: UserLogs = {LogID: data[i].date, Type: 'USER', Action: data[i].action, Date: this.getDate(data[i].dateString), Details: data[i].details, User: data[i].user, Organization1: data[i].org1, Organization2: data[i].org2, MoreInfo: data[i].moreInfo, ID: this.localNotificationNumber};
+              
+              //Getting the name and surname of the users passed using their id numbers
+              this.loadUserDetails(tempLogU.Organization1, tempLogU.Details, 'user1');
+
+              if(tempLogU.Action == "/createOrganization"){
+                tempLogU.Action = "New organization " + tempLogU.User + " was added to the system by " + this.user1;
+              }
+              else if(tempLogU.Action == "/addStaff"){
+                this.loadUserDetails(tempLogU.Organization2, tempLogU.User, 'user2');
+                tempLogU.Action = "New user, " + this.user2 + ", was added to the system by " + this.user1;
+              }
+              else if(tempLogU.Action == "/removeOrg"){
+                tempLogU.Action = "Organization " + tempLogU.User + " was removed from the system by " + this.user1;
+              }
+              else if(tempLogU.Action == "/removeStaff"){
+                this.loadUserDetails(tempLogU.Organization2, tempLogU.User, 'user2');
+                tempLogU.Action = "New user, " + this.user2 + ", was removed from the system by " + this.user1;
+              }
+  
+              this.allNotifications.push(tempLogU);
+              this.numberOfUserLogs += 1;
+              this.localNotificationNumber += 1;
+            }
+          }         
+        }
+      }
+      else{
+        //Error handling
+      }
+    });
+
+    //Making a call too the notification logging service to return all DBML logs
+    this.notificationLoggingService.getAllDatabaseManagementLogs().subscribe((response: any) => {
+      if(response.success == true){
+        //Temporarily holds the data returned from the API call
+        const data = response.data.content.data.Logs;
+
+        for(var i = 0; i < data.length; i++){
+          for(var j = 0; j < this.allLogs.length; j++){
+            if(data[i].date == this.allLogs[j]){
+              //A temporary instance of DatabaseManagementLogs that will be added to the allNotifications array
+              var tempLogD: DatabaseManagementLogs = {LogID: data[i].date, Type: 'DBML', Action: data[i].action, Date: this.getDate(data[i].dateString), Details: data[i].details, User: data[i].user, Organization1: data[i].org1, Organization2: data[i].org2, MoreInfo: data[i].moreInfo, ID: this.localNotificationNumber}
+
+              //Getting the name and surname of the users passed using their id numbers
+              this.loadUserDetails(tempLogD.Organization1, tempLogD.User, 'user1');
+
+              if(tempLogD.Action == "/createDatabase"){
+                tempLogD.Action = "New database, " + tempLogD.Details + ", was added to the system by " + this.user1;
+              }
+              else if(tempLogD.Action == "/porting"){
+                tempLogD.Action = tempLogD.Details + " was ported";
+              }
+              else if(tempLogD.Action == "C"){
+                tempLogD.Action = "New database, " + tempLogD.Details + ", was added to the system by " + this.user1;
+              }
+
+              this.allNotifications.push(tempLogD);
+              this.numberOfUserLogs += 1;
+              this.localNotificationNumber += 1;
+            }
+          }
+        }
+      }
+      else{
+        //Error handling
+      }
+    });
+  }
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                                  LOAD USER DETAILS
+  /**
+   *  This function will be called so that the information of a specific user can be fetched
+   * 
+   *  @memberof AdminNotificationComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  loadUserDetails(userOrganization: string, userID: string, type: string) {
+    //Making a call to the User Management API Service to retrieve a specific users details
+    this.userManagementService.getUserDetails(userOrganization, userID).subscribe((response: any) => {
+      if(response.success == true){
+        //Temporarily holds the data returned from the API call
+        const data = response.data;
+
+        if(type == 'user1'){
+          //Sets the users name and surname as a connected string
+          this.user1 = data.fname + ' ' + data.surname;
+        }
+        else if(type == 'user2'){
+          //Sets the users name and surname as a connected string
+          this.user2 = data.fname + ' ' + data.surname;
+        }
+      } 
+      else{
+        //Error control
+      }
+    });
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                                       REMOVE NOTIFICATIONS
+  /**
+   *  This function will remove a notification from the notification section on the HTML page.
+   * 
+   * @param {string} id                   //The id of the notification to be removed
+   * 
+   * @memberof AdminNotificationComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  removeNotification(id: string){
+    for(var i =  0; i < this.allNotifications.length; i++){
+      if(this.allNotifications[i].ID == id){
+        this.newNotifications.push(this.allNotifications[i]);
+      }
+    }
+
+    //Getting the user's details from local storage
+    var tempUser = this.authService.getCurrentUserValue;
+
+    this.notificationLoggingService.updateFABIMemberNotifications(tempUser.user.ID, this.newNotifications).subscribe((response: any) => {
+      if(response.success == true){
+        this.loadNotifications();
+      }
+      else{
+        //Error handling
+      }
+    });
+  } 
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                                      TOGGLE NOTIFICATIONS 
+  /**
+   * This function will toggle the display of the notifications side panel
+   * 
+   * @memberof AdminNotificationComponent
+   */
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  toggleNotificationsTab(){ 
+    this.notificationsTab = !this.notificationsTab;
   }
 
 }
