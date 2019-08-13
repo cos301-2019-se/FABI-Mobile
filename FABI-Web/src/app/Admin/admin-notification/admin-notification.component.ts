67451401f3872f6928f2450c5f87ae8409a446d7
Kendra <kendrar@mweb.co.za>
2019-08-13 12:52:49 +0200

    Notification Components

---
+++ FABI-Web/src/app/Admin/admin-notification/admin-notification.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-notification',
+  templateUrl: './admin-notification.component.html',
+  styleUrls: ['./admin-notification.component.scss']
+})
+export class AdminNotificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
