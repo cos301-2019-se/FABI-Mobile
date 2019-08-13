22c5440bf1e670e8683b22cd65cb40d6b56125e0
Emma <u17074526@tuks.co.za>
2019-08-13 16:48:45 +0200

    Cleaned up code

--- FABI-Web/src/app/Staff/submit-cmw-revitalization/submit-cmw-revitalization.component.ts
+++ FABI-Web/src/app/Staff/submit-cmw-revitalization/submit-cmw-revitalization.component.ts
@@ -64,27 +64,11 @@ export class SubmitCmwRevitalizationComponent implements OnInit {
   /** The date returned of the form -  @type {string} */
   dateReturned: string;
 
-  /** Object array for holding all of the logs -  @type {any[]} */ 
-  allNotifications: any[] = [];
-  /** Object array for holding all of the logs that have not been read -  @type {any[]} */ 
-  newNotifications: any[] = [];
-  /** Object array for holding all of the logs that have not been read -  @type {string[]} */ 
-  allLogs: string[] = []; 
-  
-  /** Indicates if there are notifications to load - @type {boolean} */           
-  notifications: boolean = true; 
-  /** The total number of User Logs - @type {number} */           
-  numberOfUserLogs: number = 0;
-  /** THe number of the notifications - @type {number} */   
-  localNotificationNumber : number = 1; 
-
   /** Indicates if the notifications tab is hidden/shown - @type {boolean} */   
   private toggle_status : boolean = false;
 
-  /** The name and surname of a user concatenated as a string - @type {string} */   
-  user1: string;
-  /** The name and surname of a user concatenated as a string - @type {string} */   
-  user2: string;
+  /** The details of the user currently logged in -  @type {any} */
+  currentUser: any;
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //                                                             CONSTRUCTOR
@@ -223,10 +207,7 @@ export class SubmitCmwRevitalizationComponent implements OnInit {
     var date = new Date();
     var currentDate = ('0' + date.getDate()).slice(-2) + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
 
-    //Getting the user's details from local storage
-    var tempUser = this.authService.getCurrentSessionValue;
-
-    var revitalization: CMWRevitalization = {userID: tempUser.user.ID, requestor: this.requestor, currentName: this.currentName, nameBionumerics: this.bionumericsName, cultureNumber: this.cultureNumber,
+    var revitalization: CMWRevitalization = {userID: this.currentUser.user.ID, requestor: this.requestor, currentName: this.currentName, nameBionumerics: this.bionumericsName, cultureNumber: this.cultureNumber,
       cultureCondition: this.cultureCondition, sequenceDateSubmitted: this.sequence, referenceNumber: this.referenceNumber, dateRequested: this.dateRequested,
       dateReturned: this.dateReturned, dateSubmitted: currentDate};
 
@@ -277,154 +258,6 @@ export class SubmitCmwRevitalizationComponent implements OnInit {
   }
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //                                                       LOAD LOGS
-  /**
-   *  This function will load all of the user's logs into a string array.
-   * 
-   * @memberof SubmitCmwRevitalizationComponent
-   */
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  loadLogs(){
-    //Getting the user's details from local storage
-    var tempUser = this.authService.getCurrentSessionValue;
-
-    //Making a call to the notification logging service to return all logs belonging to the user
-    this.notificationLoggingService.getUserLogs(tempUser.user.ID).subscribe((response: any) => {
-      if(response.success == true){
-        var data = response.data.content.data.Logs;
-
-        for(var i = 0; i < data.length; i++){
-          this.allLogs.push(data[i].id);
-        }
-      }
-      else{
-        //Error handling
-      }
-    });
-  }
-
-
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //                                                        LOAD NOTIFICATIONS
-  /**
-   *  This function will load the staff member's notifications into the notification section on the HTML page
-   * 
-   * @memberof SubmitCmwRevitalizationComponent
-   */
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  loadNotifications(){
-    //Loading all the logs beloning to the user
-    this.loadLogs();
-
-    //Making a call too the notification logging service to return all USER logs
-    this.notificationLoggingService.getAllUserLogs().subscribe((response: any) => {
-      if(response.success = true){
-        //Temporarily holds the data returned from the API call
-        const data = response.data.content.data.Logs;
-
-        for(var i = 0; i < data.length; i++){
-          for(var j = 0; j < this.allLogs.length; j++){
-            if(data[i].date == this.allLogs[j]){
-              //A temporary instance of UserLogs that will be added to the allNotifications array
-              var tempLogU: UserLogs = {LogID: data[i].date, Type: 'USER', Action: data[i].action, Date: this.getDate(data[i].dateString), Details: data[i].details, User: data[i].user, Organization1: data[i].org1, Organization2: data[i].org2, MoreInfo: data[i].moreInfo, ID: this.localNotificationNumber};
-              
-              //Getting the name and surname of the users passed using their id numbers
-              this.loadUserDetails(tempLogU.Organization1, tempLogU.Details, 'user1');
-              
-              if(tempLogU.Action == "/createOrganization"){
-                tempLogU.Action = "New organization " + tempLogU.User + " was added to the system by " + this.user1;
-              }
-              else if(tempLogU.Action == "/addStaff"){
-                this.loadUserDetails(tempLogU.Organization2, tempLogU.User, 'user2');
-                tempLogU.Action = "New user, " + this.user2 + ", was added to the system by " + this.user1;
-              }
-              else if(tempLogU.Action == "C"){
-                this.loadUserDetails(tempLogU.Organization2, tempLogU.User, 'user2');
-                tempLogU.Action = "New user, " + this.user2 + ", was added to the system by " + this.user1;
-              }
-              else if(tempLogU.Action == "/removeOrg"){
-                tempLogU.Action = "Organization " + tempLogU.User + " was removed from the system by " + this.user1;
-              }
-              else if(tempLogU.Action == "/removeStaff"){
-                this.loadUserDetails(tempLogU.Organization2, tempLogU.User, 'user2');
-                tempLogU.Action = "New user, " + this.user2 + ", was removed from the system by " + this.user1;
-              }
-  
-              this.allNotifications.push(tempLogU);
-              this.numberOfUserLogs += 1;
-              this.localNotificationNumber += 1;
-            }
-          }          
-        }
-      }
-      else{
-        //Error handling
-      }
-    });
-  }
-
-
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //                                                        LOAD USER DETAILS
-  /**
-   *  This function will be called so that the information of a specific user can be fetched
-   * 
-   *  @memberof SubmitCmwRevitalizationComponent
-   */
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  loadUserDetails(userOrganization: string, userID: string, type: string) {
-    //Making a call to the User Management API Service to retrieve a specific users details
-    this.userManagementService.getUserDetails(userOrganization, userID).subscribe((response: any) => {
-      if(response.success == true){
-        //Temporarily holds the data returned from the API call
-        const data = response.data;
-
-        if(type == 'user1'){
-          //Sets the users name and surname as a connected string
-          this.user1 = data.fname + ' ' + data.surname;
-        }
-        else if(type == 'user2'){
-          //Sets the users name and surname as a connected string
-          this.user2 = data.fname + ' ' + data.surname;
-        }
-      } 
-      else{
-        //Error control
-      }
-    });
-  }
-
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //                                                       REMOVE NOTIFICATIONS
-  /**
-   *  This function will remove a notification from the notification section on the HTML page.
-   * 
-   * @param {string} id                   //The id of the notification to be removed
-   * @memberof SubmitCmwRevitalizationComponent
-   */
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  removeNotification(id: string){
-    for(var i =  0; i < this.allNotifications.length; i++){
-      if(this.allNotifications[i].ID == id){
-        this.newNotifications.push(this.allNotifications[i]);
-      }
-    }
-
-    //Getting the user's details from local storage
-    var tempUser = this.authService.getCurrentSessionValue;
-
-    this.notificationLoggingService.updateFABIMemberNotifications(tempUser.user.ID, this.newNotifications).subscribe((response: any) => {
-      if(response.success == true){
-        this.loadNotifications();
-      }
-      else{
-        //Error handling
-      }
-    });
-  }
-
-
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //                                                      TOGGLE NOTIFICATIONS TAB
   /**
    *  This function is used to toggle the notifications tab.
@@ -438,82 +271,6 @@ export class SubmitCmwRevitalizationComponent implements OnInit {
   toggleNotificaitonsTab(){
     this.toggle_status = !this.toggle_status; 
   }
-
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //                                                        GET DATE
-  /**
-   *  This function will put the string date provided into a more readable format for the notifications
-   * @param {string} date The date of the log
-   * @memberof SubmitCmwRevitalizationComponent
-   */
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  getDate(date: string){
-    var tempDate = (date).split(' ');
-    var newDate = '';
-
-    newDate += tempDate[2];
-
-    if(tempDate[0] == 'Mon'){
-      newDate += ' Monday ';
-    }
-    else if(tempDate[0] == 'Tue' || tempDate[0] == 'Tu' || tempDate[0] == 'Tues'){
-      newDate += ' Tuesday ';
-    }
-    else if(tempDate[0] == 'Wed'){
-      newDate += ' Wednesday ';
-    }
-    else if(tempDate[0] == 'Thu' || tempDate[0] == 'Thur' || tempDate[0] == 'Thurs'){
-      newDate += ' Thursday ';
-    }
-    else if(tempDate[0] == 'Fri'){
-      newDate += ' Friday ';
-    }
-    else if(tempDate[0] == 'Sat'){
-      newDate += ' Saturday ';
-    }
-    else if(tempDate[0] == 'Sun'){
-      newDate += ' Sunday ';
-    }
-
-    if(tempDate[1] == 'Jan'){
-      newDate += 'January';
-    }
-    else if(tempDate[1] == 'Feb'){
-      newDate += 'February';
-    }
-    else if(tempDate[1] == 'Mar'){
-      newDate += 'March';
-    }
-    else if(tempDate[1] == 'Apr'){
-      newDate += 'April';
-    }
-    else if(tempDate[1] == 'Jun'){
-      newDate += 'June';
-    }
-    else if(tempDate[1] == 'Jul'){
-      newDate += 'July';
-    }
-    else if(tempDate[1] == 'Aug'){
-      newDate += 'August';
-    }
-    else if(tempDate[1] == 'Sep' || tempDate[1] == 'Sept'){
-      newDate += 'September';
-    }
-    else if(tempDate[1] == 'Oct'){
-      newDate += 'October';
-    }
-    else if(tempDate[1] == 'Nov'){
-      newDate += 'November';
-    }
-    else if(tempDate[1] == 'Dec'){
-      newDate += 'December';
-    }
-
-    newDate += ' ' + tempDate[3];
-
-    return newDate;
-  }
-
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //                                                                  FILTER
@@ -539,7 +296,8 @@ export class SubmitCmwRevitalizationComponent implements OnInit {
    */
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   ngOnInit() {
-    this.loadNotifications();
+    this.currentUser = this.authService.getCurrentSessionValue.user;
+    
     this.getAllStaff();
     this.filteredOptions = this.requestorControl.valueChanges.pipe(startWith(''), map(value => this._filter(value)));
   }
