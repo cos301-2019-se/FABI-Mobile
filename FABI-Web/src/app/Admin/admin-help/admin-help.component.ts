73d0b7aee608e4a7f715077601b256187f56a2bd
Emma <u17074526@tuks.co.za>
2019-08-15 14:08:04 +0200

    Added comments to new components

--- FABI-Web/src/app/Admin/admin-help/admin-help.component.ts
+++ FABI-Web/src/app/Admin/admin-help/admin-help.component.ts
@@ -1,3 +1,18 @@
+/**
+ * File Name: admin-help.component.ts
+ * File Path: c:\Users\Kendra\Documents\Varsity\Third Year\COS301\CAPSTONE\Git Repo\FABI-Mobile\FABI-Web\src\app\Admin\admin-help\admin-help.component.ts
+ * Project Name: fabi-web
+ * Created Date: Wednesday, August 14th 2019
+ * Author: Team Nova - novacapstone@gmail.com
+ * -----
+ * Last Modified: Wednesday, August 14th 2019
+ * Modified By: Team Nova
+ * -----
+ * Copyright (c) 2019 University of Pretoria
+ * 
+ * <<license>>
+ */
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
