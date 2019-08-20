fbe2c89c3a5994efac27ee8c1659802e607903df
Tegan <u17034729@tuks.co.za>
2019-08-20 14:45:38 +0200

    Admin Dashboard styling changes and Nw view Database component

---
+++ FABI-Web/src/app/staff-view-databases/staff-view-databases.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-staff-view-databases',
+  templateUrl: './staff-view-databases.component.html',
+  styleUrls: ['./staff-view-databases.component.scss']
+})
+export class StaffViewDatabasesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
