131de8ea2225e2ee2e14b5a8b3d9b6ced9c570a9
luveshanm <u17087709@tuks.co.za>
2019-08-21 23:46:50 +0200

    adding unit tests, fix karma

--- FABI-Web/src/app/Admin/admin-notification/admin-notification.component.spec.ts
+++ FABI-Web/src/app/Admin/admin-notification/admin-notification.component.spec.ts
@@ -1,13 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminNotificationComponent } from './admin-notification.component';
+//Router
+import { RouterTestingModule } from '@angular/router/testing';
+//Import form components
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+//Import the materials component
+import { MaterialModule } from '../../materials';
+//Http Testing
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+//Animation Testing
+import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DebugElement } from '@angular/core';
 
 describe('AdminNotificationComponent', () => {
   let component: AdminNotificationComponent;
   let fixture: ComponentFixture<AdminNotificationComponent>;
+  let de : DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        MaterialModule, ReactiveFormsModule, FormsModule, HttpClientTestingModule, RouterTestingModule, NoopAnimationsModule, BrowserAnimationsModule 
+      ],
       declarations: [ AdminNotificationComponent ]
     })
     .compileComponents();
@@ -16,7 +31,10 @@ describe('AdminNotificationComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AdminNotificationComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    de = fixture.debugElement;
+
+    component.ngOnInit();
+    fixture.autoDetectChanges();
   });
 
   it('should create', () => {
