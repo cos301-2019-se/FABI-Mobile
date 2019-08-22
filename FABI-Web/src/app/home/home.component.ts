a10aa28605488b56bea510be5670cdfc0fadff2a
Kendra <kendrar@mweb.co.za>
2019-08-22 09:47:33 +0200

    Organisation Request to Register

--- FABI-Web/src/app/home/home.component.ts
+++ FABI-Web/src/app/home/home.component.ts
@@ -5,7 +5,7 @@
  * Created Date: Tuesday, June 25th 2019
  * Author: Team Nova - novacapstone@gmail.com
  * -----
- * Last Modified: Saturday, August 17th 2019
+ * Last Modified: Thursday, August 22nd 2019
  * Modified By: Team Nova
  * -----
  * Copyright (c) 2019 University of Pretoria
@@ -44,6 +44,31 @@ export class HomeComponent implements OnInit {
   /** Object for storing all forms that require validation-  @type {HTMLCollectionOf<Element>} */
   forms: HTMLCollectionOf<Element> = null;
 
+  request_register_org: FormGroup;
+
+  submitted: boolean = false;
+  valid: boolean = false;
+
+  register_organization_validators = {
+    'organization_name': [
+      { type: 'required', message: 'Organization name is required' },
+    ],
+    'admin_email': [
+      { type: 'required', message: 'Email is required' },
+      { type: 'pattern', message: 'Invalid email' }
+    ],
+    'admin_name': [
+      { type: 'required', message: 'First name is required' }
+    ],
+    'admin_surname': [
+      { type: 'required', message: 'Surname is required' }
+    ],
+    'admin_phone': [
+      { type: 'required', message: 'Phone No. is required' },
+      // { type: 'pattern', message: 'Please enter a valid number' }
+    ],
+  }
+
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //                                                          CONSTRUCTOR
   /**
@@ -67,6 +92,21 @@ export class HomeComponent implements OnInit {
       name: ['', Validators.required],
       email: ['', Validators.required],
       mesage: ['', Validators.required]
+    })
+
+    this.request_register_org = this.formBuilder.group({
+      organization_name: ['', Validators.required],
+      admin_name: ['', Validators.required],
+      admin_surname: ['', Validators.required],
+      admin_email: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])],
+      admin_phone: ['', Validators.compose([
+        Validators.required,
+        // Validators.pattern('')
+      ])]
+      
     })
   }
 
@@ -102,6 +142,52 @@ export class HomeComponent implements OnInit {
   contact() {
 
 
+  }
+
+
+  requestToRegister() {
+    
+    this.submitted = true;
+
+    if (this.request_register_org.invalid) {
+      return;
+    }
+
+    this.valid = true;
+
+    let loadingRef = this.dialog.open(LoadingComponent, { data: { title: "Sending Request" } });
+
+    const LorgName = this.request_register_org.controls.organization_name.value;
+    const LadminName = this.request_register_org.controls.admin_name.value;
+    const LadminSurname = this.request_register_org.controls.admin_surname.value;
+    const LadminEmail = this.request_register_org.controls.admin_email.value;
+    const LadminPhone = this.request_register_org.controls.admin_phone.value;
+
+    const admin_details: Interface.OrganisationAdmin = { fname: LadminName, surname: LadminSurname, email: LadminEmail };
+    const org_details: Interface.Organisation = { orgName: LorgName, admin: admin_details };
+
+    this.userManagementService.sendRequestToRegisterOrganization(org_details).subscribe((response: any) => {
+
+      loadingRef.close();
+
+      if (response.success == true && response.code == 200) {
+        //POPUP MESSAGE
+
+        let snackBarRef = this.snackBar.open("Successfully Sent Request", "Dismiss", {
+          duration: 6000
+        });
+
+      } else if (response.success == false) {
+        //POPUP MESSAGE
+        let dialogRef = this.dialog.open(ErrorComponent, { data: { error_title: "Error Sending Request", message: response.message, retry: true } });
+        dialogRef.afterClosed().subscribe((result) => {
+          if (result == "Retry") {
+            this.requestToRegister();
+          }
+        })
+      }
+    });
+    
   }
 
 }
