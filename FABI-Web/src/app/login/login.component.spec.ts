0a8d7089634dc1e775fc17125c59168b1cceceb4
luveshanm <u17087709@tuks.co.za>
2019-10-07 21:28:01 +0200

    fixing login tests

--- FABI-Web/src/app/login/login.component.spec.ts
+++ FABI-Web/src/app/login/login.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
+import { LoadingComponent } from "../_loading/loading.component";
 //Router
 import { RouterTestingModule } from '@angular/router/testing';
 //Import form components
@@ -37,7 +38,8 @@ describe('LoginComponent', () => {
             
         ],
         declarations: [ 
-            LoginComponent 
+            LoginComponent,
+            LoadingComponent 
         ],
         providers :[
           { provide: ToastContainerModule, useValue: {} },
