c57e0b5ace5e85f79c0b5cd441c3aa59a542eedd
luveshanm <u17087709@tuks.co.za>
2019-10-09 03:07:14 +0200

    added more unit tests

--- FABI-Web/src/app/Admin/staff-handler/staff-handler.component.spec.ts
+++ FABI-Web/src/app/Admin/staff-handler/staff-handler.component.spec.ts
@@ -14,6 +14,7 @@ import { FilterPipe } from '../../_pipes/filter.pipe';
 //Http Testing
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AuthenticationService } from 'src/app/_services/authentication.service';
+import { UserManagementAPIService } from '../../_services/user-management-api.service';
 
 //Router
 import { RouterTestingModule } from '@angular/router/testing';
@@ -22,6 +23,8 @@ import { RouterTestingModule } from '@angular/router/testing';
 describe('StaffHandlerComponent', () => {
   let component: StaffHandlerComponent;
   let fixture: ComponentFixture<StaffHandlerComponent>;
+  let UserManagementService: UserManagementAPIService;
+  let authService : AuthenticationService;
 
   class MockAuthenticationService extends AuthenticationService{
     public get getCurrentSessionValue() {
@@ -49,11 +52,89 @@ describe('StaffHandlerComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(StaffHandlerComponent);
     component = fixture.componentInstance;
+    UserManagementService = new UserManagementAPIService( null , null);
+    authService = new AuthenticationService(null);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('get DB Names', () =>{
+    let spy = spyOn(UserManagementService, 'getDatabaseNames');
+    component.getDBNames();
+    expect(spy).toBeTruthy();
+  });
+
+  it('logging out', () =>{
+    let spy = spyOn(authService, 'logoutUser');
+    component.logout();
+    expect(spy).toBeTruthy();
+  });
+
+  it('get admin types', () =>{
+    let spy = spyOn(UserManagementService, 'getFABIAdminTypes');
+    component.getAdminTypes();
+    expect(spy).toBeTruthy();
+  });
+
+  it('add staff', () =>{
+    let spy = spyOn(UserManagementService, 'addStaffMember');
+    component.addStaff();
+    expect(spy).toBeTruthy();
+  });
+
+  it('remove staff', () =>{
+    let spy = spyOn(UserManagementService, 'removeFABIStaffMember');
+    component.removeStaffMember();
+    expect(spy).toBeTruthy();
+  });
+
+  it('refresh datasource', () =>{
+    let spy = spyOn(component, 'viewStaff');
+    component.refreshDataSource();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('view staff', () =>{
+    let spy = spyOn(UserManagementService, 'getAllStaffMembers');
+    component.viewStaff();
+    expect(spy).toBeTruthy();
+  });
+
+  it('toggle notification tab', () =>{
+    let x = component.notificationsTab;
+    component.toggleNotificationsTab();
+    expect(component.notificationsTab == !x).toBeTruthy();
+  });
+
+  it('toggle profile tab', () =>{
+    let x = component.profileTab;
+    component.toggleProfileTab();
+    expect(component.profileTab == !x).toBeTruthy();
+  });
+
+  it('toggle help tab', () =>{
+    let x = component.helpTab;
+    component.toggleHelpTab();
+    expect(component.helpTab == !x).toBeTruthy();
+  });
+
+  it('displayProfileSaveBtn', () => {
+    component.displayProfileSaveBtn();
+    expect(component.saveBtn).toBeTruthy();
+  });
+
+  it('displayConfirmPasswordInput', () => {
+    component.displayConfirmPasswordInput();
+    expect(component.confirmPasswordInput).toBeTruthy();
+  });
+
+  it('reset add fields', () =>{
+    let spy = spyOn(component.addStaffForm, 'reset');
+    component.resetAddFields();
+    expect(spy).toHaveBeenCalled();
   });
 
   it("Empty form expect invalid", () => {
@@ -63,45 +144,45 @@ describe('StaffHandlerComponent', () => {
     component.addStaffForm.controls.staff_phone.setValue("");
     component.addStaffForm.controls.staff_position.setValue("");
     component.addStaffForm.controls.admin_type.setValue("");
-    component.addStaffForm.controls.database_privileges.setValue("[]");
+    component.addStaffForm.controls.database_privileges.setValue([]);
 
     expect(component.addStaffForm.valid).toBeFalsy();
   });
 
-  it("Empty admin surname expect admin surname invalid", () => {
+  it("Empty staff surname expect admin surname invalid", () => {
     component.addStaffForm.controls.staff_name.setValue("TesterFName");
     component.addStaffForm.controls.staff_surname.setValue("");
     component.addStaffForm.controls.staff_email.setValue("");
     component.addStaffForm.controls.staff_phone.setValue("");
     component.addStaffForm.controls.staff_position.setValue("");
     component.addStaffForm.controls.admin_type.setValue("");
-    component.addStaffForm.controls.database_privileges.setValue("[]");
+    component.addStaffForm.controls.database_privileges.setValue([]);
 
-    expect(component.addStaffForm.controls.admin_surname.valid).toBeFalsy();
+    expect(component.addStaffForm.controls.staff_surname.valid).toBeFalsy();
   });
 
-  it("Empty admin email expect admin email invalid", () => {
+  it("Empty staff email expect admin email invalid", () => {
     component.addStaffForm.controls.staff_name.setValue("TesterFName");
     component.addStaffForm.controls.staff_surname.setValue("TesterLName");
     component.addStaffForm.controls.staff_email.setValue("");
     component.addStaffForm.controls.staff_phone.setValue("");
     component.addStaffForm.controls.staff_position.setValue("");
     component.addStaffForm.controls.admin_type.setValue("");
-    component.addStaffForm.controls.database_privileges.setValue("[]");
+    component.addStaffForm.controls.database_privileges.setValue([]);
 
-    expect(component.addStaffForm.controls.admin_email.valid).toBeFalsy();
+    expect(component.addStaffForm.controls.staff_email.valid).toBeFalsy();
   });
 
-  it("Empty admin phone expect admin phone invalid", () => {
+  it("Empty staff phone expect admin phone invalid", () => {
     component.addStaffForm.controls.staff_name.setValue("TesterFName");
     component.addStaffForm.controls.staff_surname.setValue("TesterLName");
     component.addStaffForm.controls.staff_email.setValue("tester@gmail.com");
     component.addStaffForm.controls.staff_phone.setValue("");
     component.addStaffForm.controls.staff_position.setValue("");
     component.addStaffForm.controls.admin_type.setValue("");
-    component.addStaffForm.controls.database_privileges.setValue("[]");
+    component.addStaffForm.controls.database_privileges.setValue([]);
 
-    expect(component.addStaffForm.controls.admin_phone.valid).toBeFalsy();
+    expect(component.addStaffForm.controls.staff_phone.valid).toBeFalsy();
   });
 
   it("Empty admin type expect admin type invalid", () => {
@@ -111,21 +192,21 @@ describe('StaffHandlerComponent', () => {
     component.addStaffForm.controls.staff_phone.setValue("0000000000");
     component.addStaffForm.controls.staff_position.setValue("clinic");
     component.addStaffForm.controls.admin_type.setValue("");
-    component.addStaffForm.controls.database_privileges.setValue("[]");
+    component.addStaffForm.controls.database_privileges.setValue([]);
 
-    expect(component.addStaffForm.controls.admin_phone.valid).toBeFalsy();
+    expect(component.addStaffForm.controls.admin_type.valid).toBeFalsy();
   });
 
-  it("Empty database privileges expect database privileges invalid", () => {
+  it("Empty database privileges expect database privileges valid", () => {
     component.addStaffForm.controls.staff_name.setValue("TesterFName");
     component.addStaffForm.controls.staff_surname.setValue("TesterLName");
     component.addStaffForm.controls.staff_email.setValue("tester@gmail.com");
     component.addStaffForm.controls.staff_phone.setValue("0000000000");
     component.addStaffForm.controls.staff_position.setValue("clinic");
     component.addStaffForm.controls.admin_type.setValue("super");
-    component.addStaffForm.controls.database_privileges.setValue("[]");
+    component.addStaffForm.controls.database_privileges.setValue([]);
 
-    expect(component.addStaffForm.controls.admin_phone.valid).toBeFalsy();
+    expect(component.addStaffForm.controls.database_privileges.valid).toBeTruthy();
   });
 
   it("Valid form expect true", () => {
@@ -135,7 +216,7 @@ describe('StaffHandlerComponent', () => {
     component.addStaffForm.controls.staff_phone.setValue("0000000000");
     component.addStaffForm.controls.staff_position.setValue("clinic");
     component.addStaffForm.controls.admin_type.setValue("super");
-    component.addStaffForm.controls.database_privileges.setValue("['Sirex']");
+    component.addStaffForm.controls.database_privileges.setValue([true]);
 
     expect(component.addStaffForm.valid).toBeTruthy();
   });
--- FABI-Web/src/app/Admin/view-forms/view-forms.component.spec.ts
+++ FABI-Web/src/app/Admin/view-forms/view-forms.component.spec.ts
@@ -16,6 +16,9 @@ import { MaterialModule } from '../../materials';
 
 //Http Testing
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { UserManagementAPIService } from '../../_services/user-management-api.service';
+import { NotificationLoggingService } from '../../_services/notification-logging.service';
+import { CultureCollectionAPIService } from '../../_services/culture-collection-api.service';
 
 //Animation Testing
 import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -23,6 +26,10 @@ import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform
 describe('ViewFormsComponent', () => {
   let component: ViewFormsComponent;
   let fixture: ComponentFixture<ViewFormsComponent>;
+  let UserManagementService: UserManagementAPIService;
+  let NotificationLogService: NotificationLoggingService;
+  let CCservice: CultureCollectionAPIService;
+  let authService : AuthenticationService;
 
   class MockAuthenticationService extends AuthenticationService{
     public get getCurrentSessionValue() {
@@ -54,10 +61,139 @@ describe('ViewFormsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ViewFormsComponent);
     component = fixture.componentInstance;
+    UserManagementService = new UserManagementAPIService( null , null);
+    NotificationLogService = new NotificationLoggingService(null, null);
+    CCservice = new CultureCollectionAPIService(null, null);
+    authService = new AuthenticationService(null);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('load user details', () => {
+    expect(component.loadUserDetails("")).toEqual('');
+  });
+
+  it('get All staff', () =>{
+    let spy = spyOn(UserManagementService, 'getAllFABIStaff');
+    component.getAllStaff();
+    expect(spy).toBeTruthy();
+  });
+
+  it('get Deposit Forms', () =>{
+    let spy = spyOn(CCservice, 'getAllDepositLogs');
+    component.getAllDepositForms();
+    expect(spy).toBeTruthy();
+  });
+
+  it('delete Deposit Forms', () =>{
+    let spy = spyOn(CCservice, 'deleteDepositForm');
+    component.deleteDepositForm();
+    expect(spy).toBeTruthy();
+  });
+
+  it('get Request Forms', () =>{
+    let spy = spyOn(CCservice, 'getAllRequestLogs');
+    component.getAllRequestForms();
+    expect(spy).toBeTruthy();
+  });
+
+  it('delete Request Forms', () =>{
+    let spy = spyOn(CCservice, 'deleteRequestForm');
+    component.deleteRequestForm();
+    expect(spy).toBeTruthy();
+  });
+
+  it('get Revitalization Forms', () =>{
+    let spy = spyOn(CCservice, 'getAllRequestLogs');
+    component.getAllRevitalizationForms();
+    expect(spy).toBeTruthy();
+  });
+
+  it('delete Revitalization Forms', () =>{
+    let spy = spyOn(CCservice, 'deleteRevitalizationForm');
+    component.deleteRevitalizationForm();
+    expect(spy).toBeTruthy();
+  });
+
+  it('get All Proccessed Forms', () =>{
+    let spy = spyOn(CCservice, 'getAllProcessedLogs');
+    component.getAllProcessedForms();
+    expect(spy).toBeTruthy();
+  });
+
+  it('delete Proccessed Forms', () =>{
+    let spy = spyOn(CCservice, 'deleteProcessedForm');
+    component.deleteProcessedForm();
+    expect(spy).toBeTruthy();
+  });
+
+  it('logging out', () =>{
+    let spy = spyOn(authService, 'logoutUser');
+    component.logout();
+    expect(spy).toBeTruthy();
+  });
+
+  it('toggle notification tab', () =>{
+    let x = component.notificationsTab;
+    component.toggleNotificationsTab();
+    expect(component.notificationsTab == !x).toBeTruthy();
+  });
+
+  it('toggle profile tab', () =>{
+    let x = component.profileTab;
+    component.toggleProfileTab();
+    expect(component.profileTab == !x).toBeTruthy();
+  });
+
+  it('toggle help tab', () =>{
+    let x = component.helpTab;
+    component.toggleHelpTab();
+    expect(component.helpTab == !x).toBeTruthy();
+  });
+
+  it('displayProfileSaveBtn', () => {
+    component.displayProfileSaveBtn();
+    expect(component.saveBtn).toBeTruthy();
+  });
+
+  it('displayConfirmPasswordInput', () => {
+    component.displayConfirmPasswordInput();
+    expect(component.confirmPasswordInput).toBeTruthy();
+  });
+
+  it('toggleDepositFormsTab', () => {
+    component.toggleDepositFormsTab();
+    expect(component.depositFormTab).toBeTruthy();
+    expect(component.requestFormTab).toBeFalsy();
+    expect(component.revitalizationFormTab).toBeFalsy();
+    expect(component.processedFormTab).toBeFalsy();
+  });
+
+  it('toggleRequestFormsTab', () => {
+    component.toggleRequestFormsTab();
+    expect(component.depositFormTab).toBeFalsy();
+    expect(component.requestFormTab).toBeTruthy();
+    expect(component.revitalizationFormTab).toBeFalsy();
+    expect(component.processedFormTab).toBeFalsy();
+  });
+
+  it('toggleRevitalizationFormsTab', () => {
+    component.toggleRevitalizationFormsTab();
+    expect(component.depositFormTab).toBeFalsy();
+    expect(component.requestFormTab).toBeFalsy();
+    expect(component.revitalizationFormTab).toBeTruthy();
+    expect(component.processedFormTab).toBeFalsy();
+  });
+
+  it('toggleProcessedFormsTab', () => {
+    component.toggleProcessedFormsTab();
+    expect(component.depositFormTab).toBeFalsy();
+    expect(component.requestFormTab).toBeFalsy();
+    expect(component.revitalizationFormTab).toBeFalsy();
+    expect(component.processedFormTab).toBeTruthy();
+  });
+
 });
